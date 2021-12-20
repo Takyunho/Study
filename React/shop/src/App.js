ea7c34396d937b72ae25308f940a7cd9b3850a53
yun <tyh1819@gmail.com>
2021-12-20 17:08:27 +0900

    쇼핑몰 프로젝트(yarn과 bootstrap 설치)

---
+++ React/shop/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
+
+// ⭐ yarn
+// npm이나 npx를 대체해서 이용할 수 있게 도와주는 친구
+// npm보다 훨씬 빠른 라이브러리 설치속도와 안정성을 자랑함
+// 일단 설치해놓으면 npx create-react-app .. 할때 자동으로 yarn이 구동되어 빨라짐
+// npm install ??? 대신 yarn add ??? 로 사용 가능 (더 빠르게 설치가능)
+// npm run start로 미리보기 띄울 때도 yarn start 이걸로 대체 가능
+
+// ⭐ Bootstrap
+// 1. react bootstrap 사이트접속
+// 2. get started 혹은 introduction 메뉴 들어가기
+// 3. npm install react-bootstrap bootstrap 어쩌구를 터미널에 설치
+// ( 이때, yarn이 설치되어 있으면 npm install대신 yarn add로 대체가능 )
+// 4. bootstrap CSS 파일 index.html 파일의 <head> 태그 안에 복붙
+
+
