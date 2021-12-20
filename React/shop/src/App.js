30b62ebf9b5c974a50457fc33f220eeb0c007f41
yun <tyh1819@gmail.com>
2021-12-20 21:41:54 +0900

    상품목록 Component화 시키기 및 반복문사용

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -1,19 +1,8 @@
+/* eslint-disable */
 import logo from './logo.svg';
 import React, { useState } from 'react';    // useState 사용을 위해 import
 import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';  // React Bootstrap에서 코드 가져올때 내가 쓸 컴포넌트 들을 import해야함
 import './App.css';
-// 변수나 함수나 자료형을 다른 파일로 저장해둔 뒤에 불러오는 방법
-// import / export 문법을 이용하여 가져와 쓸 수 있다.
-// export 문법 설명은 data.js에 기록
-
-// ⭐ import문법
-// import 변수명 from '경로';
-// 여기서 변수명은 자유롭게 작명 가능하고, 경로 쓸때 ./ 반드시 표기(현재 경로라는 의미)
-
-// ⭐ 여러개의 변수를 받으려면?
-// import { name1, name2 } from './data.js';
-// 이렇게 export 했던 변수명을 원하는 것만 골라서 써주면 됨
-// 이때, 작명 불가능하고 export했던 변수명 그대로 써야함!!
 import data from './data.js'; // 가져와서 쓰기
 
 function App() {
@@ -53,42 +42,64 @@ function App() {
 
       <div className='container'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='100%'></img>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].content} & {shoes[0].price}</p>
-          </div>
-        
-          <div className='col-md-4'>
-          <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='100%'></img>
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].content} & {shoes[1].price}</p>
-          </div>
+          {/* <Cardshoes 슈즈={shoes[0]} />
+          <Cardshoes 슈즈={shoes[1]} />
+          <Cardshoes 슈즈={shoes[2]} /> */}
 
-          <div className='col-md-4'>
-          <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='100%'></img>
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].content} & {shoes[2].price}</p>
-          </div>
-          
+          {/* props를 전달할 때 shoes라는 [ {}, {}, {} ] 이렇게 생긴 데이터를 다 전송하는게
+          아니라, 각각 카드마다 각각 다른 정보를 전달 할 수도 있다 ... */}
+          {/* 즉, 같은 컴포넌트라고 항상 같은 내용만 보여줄 수 있는게 아니다.
+          props 등을 이용해 각각 다른 내용을 전송해주면 된다.
+          그럼 같은 컴포넌트라고 해도 각각 다른 내용이 출력되는 컴포넌트를 만들 수 있다. 
+          다시 말해, 여러개의 컴포넌트를 만들 필요 없음!! */}
+
+          {/* 🔻 위에 반복되는거 map()함수 사용  */}
+          {
+            shoes.map(function (a, i) {           // 각각의 데이터인 a는 꼭 안써도 되네?
+              return (
+                <Cardshoes 슈즈={shoes[i]} i={i} key={i} /> // {shoes[i]}대신 {a} 도 가능
+              )
+            })
+          }
         </div>
       </div>
 
-
-      
     </div>
   );
 }
 
+// 컴포넌트화 하기
+// ⭐ 1. 컴포넌트로 만들고, 2.props로 데이터 전송 및 데이터 바인딩하고, 3. 반복문 돌리기
+function Cardshoes(props) {
+  return (
+    <div className='col-md-4'>
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width='100%'></img>
+      {/* src에 변수 넣을때 중괄호로 감싸고 각각 '으로 나눈 후 + */}
+      <h4>{props.슈즈.title}</h4>
+      <p>{props.슈즈.content} & {props.슈즈.price}</p>
+    </div>
+  )
+}
+// function Cardshoes(props) {
+//   return (
+//     <div className='col-md-4'>
+//       <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='100%'></img>
+//       <h4>{props.슈즈[1].title}</h4>
+//       <p>{props.슈즈[1].content} & {props.슈즈[1].price}</p>
+//     </div>
+//   )
+// }
+// function Cardshoes(props) {
+//   return (
+//     <div className='col-md-4'>
+//       <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='100%'></img>
+//       <h4>{props.슈즈[2].title}</h4>
+//       <p>{props.슈즈[2].content} & {props.슈즈[2].price}</p>
+//     </div>
+//   )
+// }
 
 
 export default App;
 
 
-// (참고)
-// src 폴더에 있는 파일들은 리액트 앱을 발행했을 때 저절로 압축이 되고 파일명이 변경되는데
-// public 폴더에 있는 파일은 리액트 앱을 발행했을 때 사이트 루트경로에 그대로 남아있습니다.
-// 그래서 / 이렇게 경로를 입력해도 잘 먹습니다.
-// 그래서 public 폴더에 있는 이미지들은 <img src="/image.jpg" /> 이렇게 쓰셔도 첨부가능합니다.
-// ★ 리액트 17버전 이상에선 public 폴더안에 있는 이미지를 CSS파일에서는
-// / image.jpg 이렇게 첨부할 수 없습니다.CSS파일에 작성할 이미지들은 src폴더 쓰셈
