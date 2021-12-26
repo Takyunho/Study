d2e16c96d1fbecdc6f414257215218134d1dd254
yun <tyh1819@gmail.com>
2021-12-26 16:37:21 +0900

    Component 여러개 중첩시 state 전달

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -12,16 +12,18 @@ import axios from 'axios';
 function App() {
 
   let [shoes, shoes변경] = useState(data);
-  let [lodingAlert, lodingAlert변경] = useState(true);
+  let [lodingAlert, lodingAlert변경] = useState(true); 
 
-    // useEffect는 컴포넌트 등장 및 업데이트시 실행되는 함수므로,
-    // Ajax를 이용해서 페이지 내용을 받아오거나 그럴 때 useEffect()함수 안에 집어넣으면 된다.
+  // 왜 App.js에 만들었을까?
+  // => 중요한 데이터는 상위 컴포넌트에 만드는게 바람직(모든 데이터는 위에서 밑으로 흐른다.)
+  let [재고, 재고변경] = useState([10, 11, 12]);
+
+
   useEffect(() => {
     axios.get()
       .then()
       .catch();
   }, []);  // 괄호 추가하면 업데이트시 ajax 요청 안하고 등장시에만 한 번 실행
-  
 
   return (
     <div className="App">
@@ -31,7 +33,6 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {/* Link태그 에러 해결 => Link태그 대신 Nav.Link태그에 as={Link} 추가  */}
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/detail/0">Detail1</Nav.Link>
               <Nav.Link as={Link} to="/detail/1">Detail2</Nav.Link>
@@ -74,33 +75,16 @@ function App() {
               }
             </div>
             <button className='btn btn-primary' onClick={() => {
-
-              // ⭐ 로딩중이라는 UI 띄우기
+              // 로딩중이라는 UI 띄우기
               {
                 lodingAlert == true
                 ? <div className='lodingAlert'><p>loding</p></div>
                 : null
               }
-              
-
-              // ⭐ post 요청
-              // 가끔은 데이터를 받아오는게 아니라 서버로 전송하기도 해야합니다.
-              // 로그인할 때, 검색할 때, 게시물을 발행할 때... 이런 경우입니다.
-              // 서버로 데이터를 전송하시려면 POST 요청을 하시면 됩니다.
-              // POST 요청은 데이터 전송할 URL과 전송할 데이터 이 두가지 항목을 입력하실 수 있습니다.
-
-              // axios.post('서버URL', { id: 'yun', pw: 1234 })
-              // .then(() => { })
-              // .catch(() => { })
-              // get 대신 post라는 함수를 쓰면 되며, URL 옆에 두번쨰 파라미터로
-              // 원하는 데이터를 입력해주면 된다. 그럼 전송됨
-              // 요청할 때 header 정보도 보낼 수 있음
-              // 사용법은 구글에 나와 있으니 필요해지면 찾아서 학습)
 
               axios.get('https://codingapple1.github.io/shop/data2.json')
                 .then((result) => {
-
-                  // ⭐ 로딩중이라는 UI 안보이게 처리
+                  // 로딩중이라는 UI 안보이게 처리
                   lodingAlert변경(false)
                   {
                   lodingAlert == true
@@ -109,23 +93,11 @@ function App() {
                   }
 
                   let 요청데이터 = result.data;
-
                   // shoes state에 ajax 요청 시 전달받은 데이터 추가하기
-                  // var 새로운신발들 = [...shoes];
-                  // 새로운신발들.push(요청데이터);
-                  // shoes변경(새로운신발들);
-                  // console.log(shoes);
-
-                  shoes변경([...shoes, ...요청데이터]);
-                  // ⭐ [{},{},{} , {},{},{}]가 됨
-                  // 1. shoes라는 기존 state 데이터를 괄호 벗겨서 여기 넣어주시고,
-                  // 2. result.data라는 ajax 성공시 받아오는 데이터도 괄호 벗겨서 여기 넣어주세요
-                  // 3. 그리고 이걸 전부 [] 대괄호로 감싸서 array를 만들어주세요 라는 뜻
-                  // 이러면 기존 state 사본생성 없이도 원하는 데이터를 한큐에 추가할 수 있음
-                  // (지금 Array 데이터를 다루고 있지만 Object 데이터들도 마찬가지로 ... 괄호벗기기 연산자 사용가능)
+                  shoes변경([...shoes, ...요청데이터]); // 이러면 기존 state 사본생성 없이도 원하는 데이터를 한큐에 추가할 수 있음
                 })
                 .catch(() => {
-                  // ⭐ 실패 UI 띄우기
+                  // 실패 UI 띄우기
                   lodingAlert변경(true)
                   {
                     lodingAlert == true
@@ -139,9 +111,8 @@ function App() {
         </Route>
 
         {/* 세부 페이지 */}
-        {/* URL 만들 땐 반복문은 안쓰고 보통 URL 파라미터 문법을 이용해 축약함 */}
         <Route path="/detail/:id">
-          <Detail shoes={shoes} />
+          <Detail shoes={shoes} 재고={ 재고 } 재고변경={ 재고변경 }/>
         </Route>
       </Switch>
 
--- React/shop/src/Detail.js
+++ React/shop/src/Detail.js
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 
 import './Detail.scss'
 
+// styled-components
 let 박스 = styled.div`
   padding : 20px;
 `;
@@ -14,32 +15,22 @@ let 제목 = styled.h4`
   font-size : 25px;
   color : ${props => props.색상};
 `;
+// styled-components 끝
 
 
 function Detail(props) {
   
-  let [alert, alert변경] = useState(true);      // ⭐ UI 만드는법 1️⃣ UI의 상태를 state로 저장(true/false)
-  let [inputData, inputData변경] = useState('');// 컴포넌트 업데이트시 진짜 실행되는지 알아보기위해 input값을 저장하는 state
+  let [alert, alert변경] = useState(true);      
 
   // useEffect 훅
   useEffect(() => {
-    
     // Detail페이지 방문 후 alert 창이 2초 후에 사라지도록
     let 타이머 = setTimeout(function () {
-      alert변경(false);                     // alert 스테이트의 true값을 false로
-      // ⭐ setTimeout 타이머를 썼으면 타이머 해제도 해야함(코드가 길어지거나 꼬이면 남아있는
-      // 타이머 때문에 이상한 현상이 일어날 수 있음)
-      // Detail컴포넌트가 사라질때 타이머를 제거하는 코드(버그를 막기위한 팁(타이머 해제 스킬))
+      alert변경(false);
       return () => { clearTimeout(타이머) } 
     }, 2000)
-
-  // ⭐ useEffect는 업데이트될 때 항상 실행됨
-  // 조건을 지정하면 업데이트마다 실행되는 것을 막을 수 있음
-  }, [alert]); // alert라는 state가 변경이 될때만 실행해줌 (대괄호 안에 state는 콤마로 여러개 넣을 수 있음)
-  // 이렇게 하면 1. Detail컴포넌트 로드가 될 때 & 2. alert라는 state가 변경이 될 때만 실행
-  // 만약 빈칸이면?(조건을 안넣으면?)
-  // 컴포넌트가 업데이트 될 때 절대 실행되지않음
-  // 즉, 페이지 로드될때만 한 번 실행하고 끝남(컴포넌트 로드때만 한번 딱 실행하고 싶은 코드를 담을 때 쓸 수 있는 일종의 트릭)
+  }, [alert]);
+  // useEffect 훅 끝
 
   let { id } = useParams();
   let history = useHistory();
@@ -53,15 +44,9 @@ function Detail(props) {
   return (
     <div className="container">
       <박스>
-        <제목 className='color'>Detail</제목>
+        <제목 className='color'>상품</제목>
       </박스>
 
-      {/* Detail 컴포넌트 업데이트 시키기 위해 만듦(<input>에다가 뭔가 입력하면 Detail 컴포넌트가 재 렌더링됨(업데이트됨)) */}
-      <input onChange={(e) => { inputData변경(e.target.value) }}></input>
-      {inputData}
-
-
-      {/* ⭐ UI만드는법 2️⃣ state가 true일때만 alert 보여주기 */}
       {
         alert === true
           ? <div className='alert-box'>
@@ -70,13 +55,6 @@ function Detail(props) {
           : null
       }
 
-
-      {/* <div className='alert-box2'>
-        <p>재고가 얼마 남지 않았습니다.</p>
-      </div>
-      <div className='alert-box3'>
-        <p>재고가 얼마 남지 않았습니다.</p>
-      </div> */}
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${findProduct.id + 1}.jpg`} width="100%" />
@@ -86,7 +64,17 @@ function Detail(props) {
           <h4 className="pt-5">{findProduct.title}</h4>
           <p>{findProduct.content}</p>
           <p>{findProduct.price}원</p>
-          <button className="btn btn-danger">주문하기</button>
+          {/* 재고 표시하기 */}
+          <Info 재고={props.재고} ></Info>
+          {/* 주문하기 클릭시 재고 변경되도록 하기 */}
+          <button className="btn btn-danger" onClick={() => { 
+            var new재고 = [...props.재고]
+            new재고[0] = new재고[0] - 1;
+            props.재고변경(new재고);
+          }}>주문하기</button>
+          {/* 함수든 변수든 부모 컴포넌트가 가진걸 자식 컴포넌트가 사용하려면
+          항상 props로 전송해서 쓸 수 있다.
+          혹은 Context 문법 or redux를 사용하면 된다. */}
           <button className="btn btn-danger" onClick={() => {
             history.goBack();
           }}>뒤로가기</button>
@@ -95,5 +83,12 @@ function Detail(props) {
     </div>
   )
 }
+
+function Info(props) {
+  return (
+    <p>남은 수량 : { props.재고[0] }</p>
+  )
+}
+
 
 export default Detail;
