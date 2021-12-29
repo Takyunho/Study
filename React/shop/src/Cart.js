3a760b7c5b0fbdb6d4e9b85f3bb377c66f146717
yun <tyh1819@gmail.com>
2021-12-29 17:03:04 +0900

    Redux2 : reducer/dispatch로 데이터 수정하는 법

--- React/shop/src/Cart.js
+++ React/shop/src/Cart.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
 import { Table } from 'react-bootstrap';
 import { connect } from 'react-redux';
@@ -18,11 +19,17 @@ function Cart(props) {
         {
           props.작명.map(function (a, i) {
             return (
-              <tr>
-                <td>{i + 1}</td>
+              <tr key={i}>
+                <td>{a.id + 1}</td>
                 <td>{props.작명[i].name}</td>
                 <td>{props.작명[i].quan}</td>
-                <td>수량변경</td>
+                {/* 버튼을 누르면 '수량증가' 요청하기 */}
+                {/* ⭐ 데이터 수정요청을 할 땐 dispatch()함수 사용
+                props.dispatch({ type : '???'}) / */}
+                <td>
+                  <button onClick={() => { props.dispatch({ type: '수량증가' }) }}>+</button>
+                  <button onClick={() => { props.dispatch({ type: '수량감소' }) }}>-</button>
+                </td>
               </tr>
             )
           })
@@ -33,63 +40,12 @@ function Cart(props) {
 
   )
 }
-// ⭐ store에 있는 state 데이터 꺼내쓰는 법
-// store 안에 있는 데이터를 props의 형태로 등록해야 사용 가능하다.
 
-// 1️⃣ 데이터 사용을 원하는 컴포넌트.js 파일 밑에 function 만들기
-// function state를props화(state){
-//   return {
-//     state : state
-//   }
-// }
-// 이 함수는 store 안에 있던 state를 props로 만들어주는 역할을 한다.
-// return 안에다가 {작명 : state}
-// 이렇게 적으면 store 안에 있던 모든 state 데이터가 props로 등록됨
-// 작명 이라는걸 출력해보면 아까 저장해뒀던 redux내의 state가 출력됨
-// { 작명 : state.name }
-// ▲ 아니면 이렇게 원하는 state만 쏙쏙 뽑아서 등록해도 됨.
 function state를props로(store) {
   return {
     작명: store
   }
 }
 
-// 2️⃣ 그 다음 export default 부분을 connect() 어쩌구를 적음
-// export default connect(state를props화)(Cart);
-// connect 함수에 위에서 만든 함수를 집어 넣음(그냥 react-redux 라이브러리 사용법)
-// Cart 컴포넌트도 함께 소괄호 안에 집어넣음
-// 그럼 redux store에 있던 데이터들이 props로 엮인 채로 컴포넌트가 export 됨
 export default connect(state를props로)(Cart);
-
-// 3️⃣ 상단에 import도 해야함
-// import { connect } from 'react-redux';
-
-
-/*
-⭐ 총정리
-- redux는 props 전송 귀찮을 때 사용한다.
-- 일단 redux를 설치부터 하고 세팅까지 완료한다.
-
-세팅은
-
-1. index.js에 <Provider>를 import 해온다.
-2. state 값공유를 원하는 컴포넌트를 감싼다.
-3. createStore를 import 해온 다음 사용법에 의해 state를 만들어
-let store라는 변수에 저장한다.
-4. <Provider store={store}> 이렇게 store를 등록하면
-Provider로 감싼 컴포넌트는 전부 store안에 있던 값을 props없이 공유 할 수 있다.
-
-
-store안에 있던 state 사용은 원하는 컴포넌트 파일로 가서
-
-1. 하단에 function state를props화() 를 하나 만들어주고 state를 props로 등록한다.(store로 해도 되네?)
-2. 그리고 또 하단에 export default connect(state를props화)(Cart); 
-이렇게 사용하면 아까 만들어둔 state가 props로 등록이 된 것임.
-props.state이름(작명한거) 이렇게 사용해서
-저장된 state를 자유롭게 사용할 수 있음
-
-세팅과정이 드러워서 그렇지
-한번 세팅하고나면 세팅완료된 모든 컴포넌트는
-redux내의 state를 자유자재로 사용가능함
-*/
 
--- React/shop/src/index.js
+++ React/shop/src/index.js
@@ -3,33 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { BrowserRouter } from 'react-router-dom';
-
-// redux 세팅하기(데이터들을 App.js 이런데가 아니라 redux를 이용해 보관하기 위해)
-// 1️⃣ 터미널에 yarn add redux react-redux 설치((redux, react-redux 두개의 라이브러리))
-// redux는 데이터를 엄격하게 관리하는 기능, react-redux는 리덕스를 리액트에서 쓸 수 있게 도와주는 기능을 제공
-// 2️⃣ 설치 후 import { Provider }
 import { Provider } from 'react-redux';
-
-// 4️⃣ createStore() 함수를 위해 import 
 import { createStore } from 'redux';
 
-// 4️⃣ redux에서 state를 하나 만들려면 createStore() 함수를 써야함 (useState가 아님)
-// import 해온 다음에 createStore(콜백함수) 로 사용
-// redux 설치 후엔 state들을 store라는 명칭으로 부름
-let store = createStore(() => {
-  // 콜백함수 안에는 내가 원하는 state 초기값을 작성
-  return [{ id: 0, name: '멋진신발', quan: 2 }, { id: 1, name: '예쁜신발', quan: 4 },
-    {id: 2, name: '좋은신발', quan: 7}]
-})
+// let store = createStore(() => {
+//   return [
+//     { id: 0, name: '멋진신발', quan: 2 },
+//     { id: 1, name: '예쁜신발', quan: 4 },
+//     { id: 2, name: '좋은신발', quan: 7 }
+//   ]
+// })
+
+// ⭐ redux에선 state 데이터의 수정방법을 미리 정의하고 수정해야한다.
+// 1. reducer 함수를 만들고 그곳에 데이터 수정하는 방법을 정의해놓습니다.
+// 2. 그리고 원하는 곳에서 dispatch() 라는 함수를 써서 reducer에게 수정해달라고 요청을 합니다.
+
+// 따로 state 변수를 만들어 두고 이걸 reducer에 default 파라미터 문법으로 집어넣기
+// default 파라미터 문법은 ES6 신문법으로, 기본으로 가질 파라미터를 부여할 수 있음
+// 그냥 파라미터 선언할 때 = 등호로 입력하면 됨
+let 초기값 = [
+  { id: 0, name: '멋진신발', quan: 2 },
+  { id: 1, name: '예쁜신발', quan: 4 },
+  { id: 2, name: '좋은신발', quan: 7 }
+]
+
+// 데이터 수정하는 법은 reducer로 만드는데,
+// 그냥 function 어쩌구로 시작하는 흔히 보이는 함수로 만들면됨.
+// 대신 reducer는 function안에 1. state 초기값과 2. state 데이터 수정방법이 들어있음
+function reducer(state = 초기값, 액션) {
+  if (액션.type === '수량증가') { //(액션.type === 수정방법이름) 이런 식
+    
+    // state 수정
+    let copy = [...state];
+    copy[0].quan++;
+    // 수정된 state 리턴 
+    return copy
+
+  } else if (액션.type === '수량감소'){
+    
+    let copy = [...state]
+    copy[0].quan--;
+    return copy    
+
+  } else {
+    // 기본 state (reducer는 항상 state를 뱉어야함)
+    return state
+  }
+}
+
+// 위에서 만든 reducer를 createStore()안에 넣음
+let store = createStore(reducer);
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* 3️⃣ 내가 state값 공유를 원하는 컴포넌트를 <Provider>로 감싸기 */}
-      <Provider store={store}> { /* 5️⃣ props처럼 등록*/}
+      <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
@@ -42,4 +73,22 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// redux 쓰는 이유 : props 전송 없이도 모든 컴포넌트들이 state를 사용할 수 있게 만들어줌
+// ⭐ redux 쓰는 이유
+// 1. props 전송 없이도 모든 컴포넌트들이 state를 사용할 수 있게 만들어줌
+// 2. state 데이터 관리가 용이하다.
+//  - 데이터의 수정방법을 미리 정의해둘 수 있다는게 장점
+
+// 지금 같은 소규모 사이트에선 전혀 필요가 없음
+// 그러나 대규모 사이트들에서는 데이터를 한 눈에, 한 곳에 관리할 수 있어서 사용함
+
+// 대규모 사이트들은 컴포넌트가 무수히 많은데, 무수히 많은 컴포넌트에서 장바구니 state를
+// 수정하는 코드를 짜게 되었을 때, 중간에 state 하나에 이상한 값이 들어와서 버그가 생기면?
+// 버그를 찾으려고 state 수정하기위해 컴포넌트를 다 뒤져야 함
+
+// 근데 Redux를 만들어 state 수정하는 방법을 미리 정해놓으면
+// redux 안의 reducer만 잘 들여다 보면 됨. (혹은 간혹가다 dispatch부분도)
+// 그래서 사용함
+
+// 그래서 리덕스를 쓰면 'state 관리가 용이하다'고 함
+// 즉, 상태관리가 용이하다
+// 서버의 데이터입출력 API 만드는 과정이랑 유사하다고 이해하면 됨
