1531efaeb3761e9e0e13a34f3817d474479abaed
yun <tyh1819@gmail.com>
2022-01-03 14:29:37 +0900

    state 변경함수 사용할 때 주의점 : async

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -28,6 +28,7 @@ function App() {
   //     .catch();
   // }, []);  // 괄호 추가하면 업데이트시 ajax 요청 안하고 등장시에만 한 번 실행
 
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
@@ -51,7 +52,6 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
 
       <Switch>
         {/* 메인 페이지 */}
@@ -140,7 +140,7 @@ function Cardshoes(props) {
     // history를 이용하여 상세 페이지로 이동되게끔
     // props.i를 쓰면 상품을 정렬 했을 경우 문제가 발생 -> 따라서 상품의 고유 번호(id)로 하자.
     <div className='col-md-4' onClick={() => { history.push('/detail/' + props.슈즈.id) }}>
-        <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width='100%'></img>
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width='100%'></img>
       <h4>{props.슈즈.title}</h4>
       <p>{props.슈즈.content} & {props.슈즈.price}</p>
       <Test i={props.i}></Test>
@@ -162,227 +162,172 @@ function Test(props) {
 export default App;
 
 
-// React에서 자주쓰는 if문 작성패턴
+// state 변경함수 사용할 때 주의점 : async
 
-// 1️⃣ 컴포넌트 안에서 쓰는 if/else
+/*
+✅ 자바스크립트의 sync / async 관련 상식
 
-// function Component() {
-//   if ( true ) {
-//     return <p>참이면 보여줄 HTML</p>;
-//   } else {
-//     return null;
-//   }
-// }
+자바스크립트는 일반적인 코드를 작성하면 synchonous(동기방식) 하게 처리된다.
+무슨 소리냐면 코드 적은 순서대로 윗줄부터 차례로 코드가 실행된다는 뜻
+실은 거의 모든 프로그래밍 언어들은 무조건 위에서부터 한줄한줄 실행됨
 
-// 컴포넌트에서 JSX를 조건부로 보여주고 싶으면 위에처럼 씀
-// 자바스크립트 if문은 return () 안의 JSX 내에서는 사용이 불가능
-// 그래서 보통 return + JSX 전체를 출력하는 if문을 작성해서 사용한다.
-//   그런데,
-//   위의 컴포넌트는 else와 중괄호를 생략할 수 있음
+예를 들어,
+console.log(1+1)
+console.log(1+2)
+console.log(1 + 3)
 
-//   function Component() {
-//     if ( true ) {
-//       return <p>참이면 보여줄 HTML</p>;
-//     }
-//     return null;
-// }
-  
-// 왜냐면 자바스크립트 function 안에선 return 이라는 키워드를 만나면 return 밑에 있는
-// 코드는 더이상 실행되지 않기 때문
-// if -> else if -> else 이렇게 구성된 조건문도 if 두개로 축약가능
+이런 코드는 그냥 위에서부터 한줄한줄 잘 실행됨
+그래서 콘솔창에 2, 3, 4 순으로 출력
 
+자바스크립트는 이상한 함수들을 사용하면 asynchronous(비동기적)하게 코드실행이 가능하다.
+ajax, 이벤트리스너, setTimeout 이런 함수들을 쓸 때 그런 현상이 일어난다.
+이런 함수들은 처리시간이 오래걸림
+ajax를 예로 들면 인터넷 상황이 안좋으면 코드 실행이 오래 걸림. 10초도 걸릴 수 있음.
+그래서 ajax 요청하는 코드들은 순차적으로 실행되지 않고 완료되면 실행됨
 
-// 2️⃣ JSX안에서 쓰는 삼항연산자
+예를 들어,
+console.log(1+1)
+axios로 get요청하고나서 console.log(1+2) 실행해주셈~
+console.log(1+3)
 
-// 영어로 ternary operator 라고 한다.
-//   조건문 ? 조건문 참일때 실행할 코드: 거짓일 때 실행할 코드
-// 이 형식에 맞춰 쓰면 끝
+이런 코드는 2가 출력되고 4가 출력되고 그 다음에 3이 출력됨
+3을 출력하는 코드가 asynchronous 처리를 지원하는 코드라 그런거
+3을 출력할 때 오래걸리면 완료될 때 까지 잠깐 보류했다가
+다른 코드를 먼저 실행시킨다는 소리 2 => 4 => 3
+심지어 ajax요청이 0.00초 걸려도 4가 먼저, 그 다음 3이 출력
+물리적으로 잠깐 처리가 보류되어서 그런거
+자바스크립트라는 언어의 특징이자 장점이라고 볼 수 있다.
 
-// function Component() {
-//   return (
-//     <div>
-//       {
-//         1 === 1
-//         ? <p>참이면 보여줄 HTML</p>
-//         : null
-//       }
-//     </div>
-//   )
-// }
+*/
+/*
+✅ 리액트의 setState 함수 특징 
 
-// 그냥 JSX 내에서 if/else 대신 쓸 수 있다는게 장점
-// 삼항연산자는 그냥 if와는 다르게 JSX 안에서도 실행가능하며 조건을 간단히 주고 싶을 때 사용
-// 삼항연산자는 중첩 사용도 된다. 🔻
+리액트로 state 만들 땐 이렇게 합니다.
 
-// function Component() {
-//   return (
-//     <div>
-//       {
-//         1 === 1
-//         ? <p>참이면 보여줄 HTML</p>
-//         : ( 2 === 2
-//             ? <p>안녕</p>
-//             : <p>반갑</p>
-//           )
-//       }
-//     </div>
-//   )
-// }
+function App(){
+  let [name, setName] = useState('kim');
+}
 
-// else 문 안에 if/else 문을 하나 추가 한 것
-// 그러나 보기 안좋고 헷갈릴 수 있으므로
-// 그냥 return문 바깥에서 if else 쓴 다음 그 결과를 변수로 저장해놓고
-// 변수를 저기 집어넣든가 해서 사용하는 것이 낫다.
+그리고 이제 setName을 사용하면 name이라는 state를 자유롭게 변경할 수 있습니다.
+setName('park') 이런 식으로 하면 변경된다는 겁니다.
+근데 문제는 setName() 같은 state 변경함수들은
+전부 asynchronous (비동기적) 으로 처리됩니다.
+즉, setName()이 오래걸리면 이거 제껴두고 다른 밑에 있는 코드들부터 실행한다는 것입니다.
+그래서 뭔가 예상치 못한 문제가 생길 수 있습니다.
 
 
-// 3️⃣ && 연산자로 if 역할 대신하기
+🔻 예제 : 버튼을 누르면 2개 기능을 순차적으로 실행하고 싶습니다.
 
-// if문 쓸때
-// '만약에 이 변수가 참이면 <p></p>를 이 자리에 뱉고 참이 아니면 null 뱉고'
-// 이런 패턴이 자주 쓰임
-// 이걸 && 를 써서 조금 더 쉽게 축약할 수 있음
+버튼을 누를 때마다
 
-// function Component() {
-//   return (
-//     <div>
-//       {
-//         1 === 1
-//         ? <p>참이면 보여줄 HTML</p>
-//         : null
-//       }
-//     </div>
-//   )
-// }
-
-// function Component() {
-//   return (
-//     <div>
-//       {
-//         1 === 1 && <p>참이면 보여줄 HTML</p>
-//       }
-//     </div>
-//   )
-// }
-
-// 위의 두 예제는 동일한 역할을 한다.
-// 밑의 예제는 왼쪽 조건식이 true면 오른쪽 JSX가 그 자리에 남게된다.
-// 왼쪽 조건식이 false면 false가 남는다.(false면 HTML로 렌더링하지 않음)
+(1) count라는 state를 +1 해야합니다. (버튼누른 횟수 기록용)
+(2) age라는 state도 +1 해야합니다. 
+(3) 근데 count 가 3 이상이면 더 이상 age라는 state를 1 더하지 말도록 코드를 짜십시오.
+버튼 3번 이상 누르면 (count가 3 이상이면) 나이를 그만더하라는 기능입니다.
+그니까 22살에서 멈춰야합니다.
 
 
-// (참고)
-// 자바스크립트에서의 && 연산자 = 왼쪽 오른쪽 둘다 true면 전체를 true로 바꿔줘 라는 뜻
+function App(){
+  let [count, setCount] = useState(0);
+  let [age, setAge] = useState(20);
 
-// true && false;  => false가 남음
-// true && true;   => true가 남음
+  return (
+    <div>
+        <div>안녕하십니까 전 {age}</div>
+        <button onClick={() => {
+          
+          setCount(count + 1);
+          if (count < 3) {
+            setAge(age + 1);
+          }
+        
+        }}>누르면한살먹기 </button>
+          </div>
+  )
+}
 
-// 근데 자바스크립트에는 이상한 현상이 있다.
-// && 기호로 비교할 때 true와 false를 넣는게 아니라 자료형을 넣으면,
-  
-// true && '안녕'; => '안녕'이 남음
-// false && '안녕';  => false 가 남음
+1. 버튼을 누르면 count를 +1 해줍니다. 버튼누른 횟수 기록용이니까요.
+2. 그리고 만약에 count라는게 3회보다 적으면 age를 +1 해줍니다.
+끝입니다. 그러면 아마 count라는게 2일 때 까지 실행해주니까
+age는 20에서 22가 되면 더이상 증가하지 않고 멈추겠군요. 
 
-// 즉 && 기호를 중첩해서 여러개 쓸 때 false가 먼저오면 false값을 찾아주고 그게 아니면
-// 마지막 값을 남겨줌
+근데 23까지 증가하는데? 
+뭔가 이상합니다. 
+분명 count가 2일 때까지만 age를 +1 해주라고 했습니다.
+count가 1일 때 age +1
+count가 2일 때 age +1
+count가 3이면 age +1 하지마 이런 코드니까요. 
+근데 지금은 count가 3일 때도 age +1를 해주고 있는 듯 합니다. 
+왜죠? 
 
+이유는 위에서 말한 async라는 특징 때문에 그렇습니다. 
+state 변경함수는 async 하게 처리되는 함수기 때문에
+완료되기까지 시간이 오래걸리면 제쳐두고 다음 코드를 실행해줍니다.
+그래서 코드를 해석해보자면 
 
-// 4️⃣ switch / case 조건문
+① 버튼을 세번째 누르면 setCount(count+1); 이걸 실행해서 count를 3을 만들어줍니다.
+② 근데 count를 3으로 만드는건 오래걸리니까 제껴두고 if ( count > 3 ) {} 이걸 실행합니다.
+③ 이 때 count는 아직 2라서 if문 안의 setAge(age+1)이 잘 동작하고 있는겁니다.
 
-// if문이 중첩해서 여러개 달려있는 경우에 사용
+이 모든 문제는 setCount()가 async 함수라서 그렇습니다. 
+async함수는 오래걸리면 제껴두고 다음 줄 코드부터 실행하니까 그렇습니다.
 
-// function reducer(state, 액션){
-  
-//   if (액션.type === '수량증가'){
-//     return 수량증가된state
-//   } else if (액션.type === '수량감소'){
-//     return 수량감소된state
-//   } else {
-//     return state
-//   }
-// }
-// 이런식으로 썼었는데, (index.js의 reducer에서)
-// 이걸 아래처럼 쓸 수 있음
+그래서 저렇게 state1 변경하고나서 state2를 변경하는 코드를 작성할 땐 가끔 문제가 생깁니다.
+이걸 정확히 sync스럽게, 순차적으로 실행하고 싶을 때 해결책은 useEffect입니다. 
 
-// function reducer(state, 액션){
-  
-//   switch (액션.type) {
-//     case '수량증가' :
-//       return 수량증가된state;
-//     case '수량감소' :
-//       return 수량감소된state;
-//     default :
-//       return state
-//   }
+App 컴포넌트안에 useEffect를 만들어봅니다.
 
-// }
-
-// 🔺 switch는 어떻게 쓰냐면
-// 1. switch (검사할변수명){} 이거부터 작성하고
-// 2. 그 안에 case 검사할변수명이 이거랑 일치하냐 : 를 넣어줌.(이게 if문)
-// 3. 그래서 이게 일치하면 case : 밑에 있는 코드를 실행
-// 4. default : 는 그냥 맨 마지막에 쓰는 else문과 동일함
-
-// 장점은 ... if문 연달아쓸 때 코드가 약간 줄어들 수 있음. (괄호도 줄고)
-
-
-// 5️⃣ 오브젝트 자료형을 응용한 enum
-// '경우에 따라서 다른 HTML을 보여주고 싶은 경우'
-// if문 여러개 혹은 삼항연산자 여러개를 작성했는데,
-// 이렇게 작성할 수도 있음
-
-// 예를 들면 쇼핑몰에서 상품설명부분을 탭으로 만든다고 합시다.
-// 그리고 경우에 따라서 상품정보 / 배송정보 / 환불약관 내용을 보여주고 싶은겁니다.
-
-// 현재 state가 info면 <p>상품정보</p>
-// 현재 state가 shipping이면 <p>배송정보</p>
-// 현재 state가 refund면 <p>환불약관</p>
-
-// 이런걸 보여주자는겁니다.
-// state를 만들어놓고 if문으로 state를 검사하는 문법을 써야할 것 같지만
-// 이번엔 if문이 아니라 자바스크립트 오브젝트자료형에 내가 보여주고 싶은 HTML을 다 담습니다.
+useEffect(()=>{
+    
+ }, [count]) 
 
 
-// function Component() {
-//   var 현재상태 = 'info';
-//   return (
-//     <div>
-//       {
-//         { 
-//            info : <p>상품정보</p>,
-//            shipping : <p>배송관련</p>,
-//            refund : <p>환불약관</p>
-//         }[현재상태]
-//       }
+useEffect는 컴포넌트가 렌더링/재렌더링될 때 실행되는 함수랬습니다.
+근데 뒤에다가 [] 대괄호안에 state를 집어넣으면
+state가 변경되면 이 코드 실행해주세요~ 라는 뜻으로도 사용가능합니다.
+그래서 이거 쓰시면 아까 말했던 문제를 해결할 수 있습니다. 
 
-//     </div>
-//   )
-// } 
+1. count라는 state가 변경되고나서 2. age도 변경해주세요~ 이런 식으로
+순차적으로 코드를 실행할 수 있다는 것입니다.
 
-// ▲ 원래 JSX는 저렇게 오브젝트에 담든, 어레이에 담든 아무 상관없습니다.
-// 암튼 이렇게 object 자료형으로 HTML을 다 정리해서 담은 다음
-// 마지막에 object{ } 뒤에[] 대괄호를 붙여서
-// "key값이 현재상태인 자료를 뽑겠습니다" 라고 써놓는겁니다.
-// 그럼 이제 현재상태라는 변수의 값에 따라서 원하는 HTML을 보여줄 수 있습니다. 
-// 만약에 var 현재상태가 'info'면 info 항목에 저장된 <p>태그가 보여질 것이고
-// 만약에 var 현재상태가 'refund'면 refund 항목에 저장된 <p>태그가 보여지겠죠? 
+① 그래서 일단 버튼을 이렇게 변경했습니다. 
 
-// 혹은 더욱 간지나게 오브젝트를 변수로 저장해놓고 쓰셔도 무방합니다.
+<button onClick={()=>{
+
+  setCount(count+1);
+
+}}>누르면한살먹기</button> 
+
+count라는 것만 +1 되게 바꿨습니다. 
 
 
-// var 탭UI = { 
-//   info : <p>상품정보</p>,
-//   shipping : <p>배송관련</p>,
-//   refund : <p>환불약관</p>
-// }
+② 그 다음에 나머지 age를 +1 하는 코드는 useEffect안에 개발해놨습니다.
 
-// function Component() {
-//   var 현재상태 = 'info';
-//   return (
-//     <div>
-//       {
-//         탭UI[현재상태]
-//       }
-//     </div>
-//   )
-// } 
-// ▲ 뭔가 매우 깔끔해졌습니다.
-// (예제에선 귀찮아서 state가 아니라 var 변수를 만들었습니다)
+useEffect(()=>{
+  if ( count < 3 ) {
+    setAge(age+1)
+  }
+ }, [count]) 
+이런 식입니다.
+
+그러면 useEffect는 count라는 state가 변경되고나서 실행이 되며
+그럼 if문으로 count라는 state값을 제대로 의도대로 측정해볼 수 있는 겁니다.
+
+③ 근데 문제는 useEffect 저렇게 써도 처음 페이지 로드될 때도 한번 실행이
+되기 때문에 의도치 않은 버그가 발생할 수 있습니다. 
+그래서 처음 페이지 로드시 useEffect 실행을 막는 코드를 알아서 검색해서 적용하셔도 되고
+아니면 count라는 state를 또 활용하셔도 됩니다. 
+count가 0일 때는 (페이지 처음 로드되었을 때는) 내부 코드를 동작시키지 않으면 될듯요
+
+useEffect(()=>{
+  if ( count != 0 && count < 3 ) {
+    setAge(age+1)
+  }
+ }, [count])
+
+이런 식입니다. count가 0이 아닐 때만 실행하라고 조건을 추가해줬습니다.
+이제 버튼 누르면 22살까지만 잘 증가합니다.
+
+
+
+*/
