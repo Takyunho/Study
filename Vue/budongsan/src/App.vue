43951fcdf9859a85958f4cc0b16847802793ab19
yun <tyh1819@gmail.com>
2022-03-14 20:45:44 +0900

    3. Vue 반복문 v-for

--- Vue/budongsan/src/App.vue
+++ Vue/budongsan/src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h4>뿌동산</h4>
-  <div class="margin">
-    <div class="font-color"> {{ products[0] }} </div>
-    <div> {{ price1 }}만원 </div>
+  
+  <nav class="menu">
+    <a v-for="a in navMenu" :key="a"> {{ a }} </a>
+  </nav>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <h4>부동산 사이트 만들기</h4>
+  <!-- <div class="content">
+    <div class="content-title"> {{ products[0] }} </div>
+    <div> {{ price0 }}만원 </div>
   </div>
-  <div class="margin">
-    <div class="font-color"> {{ products[1] }} </div>
+  <div class="content">
+    <div class="content-title"> {{ products[1] }} </div>
+    <div> {{ price1 }} 만원 </div>
+  </div>
+  <div class="content">
+    <div class="content-title"> {{ products[2] }} </div>
     <div> {{ price2 }} 만원 </div>
+  </div> -->
+  <div class="content" v-for="(a,i) in products" :key="i">
+    <div class="content-title"> {{ products[i] }} </div>
+    <div> {{ price[i] }}만원 </div>
   </div>
-  <div class="margin">
-    <div class="font-color"> {{ products[2] }} </div>
-    <div> {{ price3 }} 만원 </div>
-  </div>
+  
 </template>
 
 <script>
@@ -22,10 +32,10 @@ export default {
   name: 'App',
   data(){
     return{
-      price1 : 50,
-      price2 : 40,
-      price3 : 70,
+      price : [50, 40, 70], 
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      navMenu : ['Home', 'Shop', 'MyPage'],
+
     }
   },
   components: {
@@ -41,17 +51,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 
-.margin {
+.content {
   margin-top: 20px;
   letter-spacing: 1px;
   padding: 5px;
 }
-.font-color {
+.content-title {
   font-size: 20px;
   color: rgb(29, 29, 173);
 }
 
+.menu {
+  background: lightskyblue;
+  padding: 16px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
 </style>
