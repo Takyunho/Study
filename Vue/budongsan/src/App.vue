a2cfe0317fd5f1feea62d528ccc773d8391bbc7e
yun <tyh1819@gmail.com>
2022-03-14 22:58:34 +0900

    4. Vue에서의 이벤트 핸들러(click)

--- Vue/budongsan/src/App.vue
+++ Vue/budongsan/src/App.vue
@@ -6,22 +6,28 @@
 
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <h4>부동산 사이트 만들기</h4>
-  <!-- <div class="content">
+  <div class="content">
     <div class="content-title"> {{ products[0] }} </div>
-    <div> {{ price0 }}만원 </div>
+    <div> {{ price[0] }}만원 </div>
+    <button @click="increase">허위매물신고</button>
+    <span>신고수 : {{신고수[0]}} </span>
   </div>
   <div class="content">
     <div class="content-title"> {{ products[1] }} </div>
-    <div> {{ price1 }} 만원 </div>
+    <div> {{ price[1] }} 만원 </div>
+    <button @click="신고수[1]++">허위매물신고</button>
+    <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div class="content">
     <div class="content-title"> {{ products[2] }} </div>
-    <div> {{ price2 }} 만원 </div>
-  </div> -->
-  <div class="content" v-for="(a,i) in products" :key="i">
-    <div class="content-title"> {{ products[i] }} </div>
-    <div> {{ price[i] }}만원 </div>
+    <div> {{ price[2] }} 만원 </div>
+    <button @click="신고수[2]++">허위매물신고</button>
+    <span>신고수 : {{ 신고수[2] }}</span>
   </div>
+  <!-- <div class="content" v-for="(a,i) in products" :key="i">
+    <div class="content-title"> {{ a }} </div>
+    <div> {{ price[i] }}만원 </div>
+  </div> -->
   
 </template>
 
@@ -35,9 +41,17 @@ export default {
       price : [50, 40, 70], 
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       navMenu : ['Home', 'Shop', 'MyPage'],
-
+      신고수 : [0,0,0],
     }
   },
+  // 함수는 여기에다가
+  methods: {
+    increase(){
+      // this.데이터이름
+      // 메소드 안에서 this는 메소드를 가지고 있는 오브젝트를 뜻한다.
+      this.신고수[0]++;
+    }
+  },  // , 잊지말자
   components: {
     
   }
