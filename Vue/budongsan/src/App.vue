d2d6d87e9fbef010e4eefdf6c496346962bb4571
yun <tyh1819@gmail.com>
2022-03-14 19:59:08 +0900

    2. Vue 데이터 바인딩

--- Vue/budongsan/src/App.vue
+++ Vue/budongsan/src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h4>뿌동산</h4>
+  <div class="margin">
+    <div class="font-color"> {{ products[0] }} </div>
+    <div> {{ price1 }}만원 </div>
+  </div>
+  <div class="margin">
+    <div class="font-color"> {{ products[1] }} </div>
+    <div> {{ price2 }} 만원 </div>
+  </div>
+  <div class="margin">
+    <div class="font-color"> {{ products[2] }} </div>
+    <div> {{ price3 }} 만원 </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      price1 : 50,
+      price2 : 40,
+      price3 : 70,
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -23,4 +43,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.margin {
+  margin-top: 20px;
+  letter-spacing: 1px;
+  padding: 5px;
+}
+.font-color {
+  font-size: 20px;
+  color: rgb(29, 29, 173);
+}
+
 </style>
