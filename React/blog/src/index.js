7723b34418b6b6bcafd1f08a75736ebe65d667bc
Takyunho <tyh1819@gmail.com>
2022-05-17 12:32:57 +0900

    react, reactDOM 설명 추가

--- React/blog/src/index.js
+++ React/blog/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';  // react 라이브러리는 컴포넌트를 만드는 역할
+import ReactDOM from 'react-dom'; // React를 <script> 태그로 불러온다면 ReactDOM 글로벌 영역에서 상위 레벨 API를 이용할 수 있습니다. npm과 ES6를 사용한다면 import ReactDOM from 'react-dom'로 쓸 수 있습니다.
+// react-dom 라이브러리는 컴포넌트를 render하고 dom에 그려주는 역할(HTMLElement에 연결하는 역할)
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
