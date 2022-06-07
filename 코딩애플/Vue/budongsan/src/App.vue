22d9546dfc0bfe0ed3ad0251290ba3ce40adbb23
Takyunho <tyh1819@gmail.com>
2022-06-07 17:39:58 +0900

    1. v-model 사용법 2. :class를 이용한 class 조건부로 넣기 3. transition 태그 이용 4. watch 5. sort함수 및 filter를 이용한 정렬기능(+spread operator)

--- 코딩애플/Vue/budongsan/src/App.vue
+++ 코딩애플/Vue/budongsan/src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <!-- 모달창 -->
-  <ModalWindow
-    :oneroomData="oneroomData"
-    :ModalOnOff="ModalOnOff"
-    :detailCount="detailCount"
-    @ModalOnOff="ModalOnOff = false"
-  />
+  <!-- <div class="modal-visible-start" :class="{modalVisibleEnd : ModalOnOff}"> -->
+    <!-- css로 애니메이션 주기 -->
+    <!--
+      :class="{ 작명 : true or false}" 
+      조건부로 class명을 부착할 수 있다. 작명이 true이면 class가 부착된다.
+    -->
+    <!-- Vue에서 제공하는 <transition name="작명"> 태그 이용하기 -->
+    <transition name="modalVisible">
+      <ModalWindow
+        :oneroomData="oneroomData"
+        :ModalOnOff="ModalOnOff"
+        :detailCount="detailCount"
+        @ModalOnOff="ModalOnOff = false"
+      />  
+    </transition>
+  <!-- </div> -->
   <!-- <ModalWindow :작명="하단의데이터이름" /> 이런 식으로 작성해서 props를 하위 컴포넌트(또는 자식 컴포넌트)로 전송 -->
   <!-- 콜론(:)의 역할 1. 데이터 바인딩 2. props 전송 -->
 
@@ -17,6 +27,12 @@
   <!-- 컴포넌트 사용 -->
   <DiscountBanner />
 
+  <!-- 정렬 버튼 -->
+  <button @click="priceSort()">가격순 정렬하기</button>
+  <button @click="priceSortReverse()">가격 역순 정렬하기</button>
+  <button @click="sortABC()">가나다순 정렬하기</button>
+  <button @click="priceFilter()">50만원 이하만 보기</button>
+  <button @click="sortBack()">되돌리기</button>
   <!-- content -->
   <h4>부동산 사이트 만들기</h4>
   <CardBox
@@ -44,19 +60,46 @@ export default {
       price: [50, 40, 70],
       // products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       oneroomData: data,
+      oneroomDataOrigin: [...data], // 원본 데이터를 보존하기 위해 새로 만든것(spread operator 사용) 
       navMenu: ["Home", "Shop", "MyPage"],
       ModalOnOff: false,
       detailCount: 0,
     };
   },
   // 함수는 여기에다가
-  // methods: {
-  //   increase(i) {
-  //     // this.데이터이름
-  //     // 메소드 안에서 this는 메소드를 가지고 있는 오브젝트를 뜻한다.
-  //     this.goodCount[i]++;
-  //   },
-  // }, // 쉼표 잊지말자
+  methods: {
+    // 오름차순으로 정렬하기
+    priceSort() {
+      this.oneroomData.sort(function (a, b) {
+        return a.price - b.price 
+      })
+      // sort함수를 사용하면 원본이 변형되므로, 원본을 따로 보존하는 식으로 개발하는 것이 바람직하다.
+    },
+    // 내림차순으로 정렬하기
+    priceSortReverse() {
+      this.oneroomData.sort(function (a, b) {
+        return b.price - a.price
+      })
+    },
+    // 가나다순으로 정렬하기
+    sortABC() {
+      this.oneroomData.sort(function (a, b) {
+        // return a.title < b.title ? -1 : a.title > b.title ? 1 : 0;
+        return a.title.localeCompare(b.title)
+      })
+    },
+    // 50만원 이하만 보여주기
+    priceFilter() {
+      this.oneroomData = this.oneroomData.filter(function (a) {
+        return a.price < 500000;
+      })
+    },
+    // 되돌리기
+    sortBack() {
+      this.oneroomData = [...this.oneroomDataOrigin];   // data원본을 sort때문에 변한 oneroomData에 재할당해서 원본으로 되돌리는 것
+    }
+
+  }, // 쉼표 잊지말자
   components: {
     // 컴포넌트 사용을 위해 등록하기
     DiscountBanner: DiscountBanner,
@@ -130,5 +173,40 @@ div {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+/* class를 이용하여 모달창에 애니메이션 주기 */
+/* .modal-visible-start {
+  opacity: 0;
+  transition: all 1s;
+}
+
+.modalVisibleEnd {
+  opacity: 1;
+} */
+
+/* transition 태그를 이용하여 모달창에 애니메이션 주기(모달창 열때)*/
+.modalVisible-enter-from {
+  /* 작명-enter-from { 애니메이션 동작 전 상태 } */
+  opacity: 0;
+}
+.modalVisible-enter-active {
+  /* 작명-enter-active { 애니메이션 동작 중 상태, 대부분 transition } */
+  transition: all 1s;
+}
+.modalVisible-enter-to {
+  /* 작명-enter-to { 애니메이션 동작 후 상태 } */
+  opacity: 1;
+}
+
+/* 퇴장시 애니메이션 주기(모달창 닫을때) */
+.modalVisible-leave-from {
+  opacity: 1;
+}
+.modalVisible-leave-active {
+  transition: all 1s;
+}
+.modalVisible-leave-to {
+  opacity: 0;
 }
 </style>
--- 코딩애플/Vue/budongsan/src/components/ModalWindow.vue
+++ 코딩애플/Vue/budongsan/src/components/ModalWindow.vue
@@ -5,7 +5,10 @@
       <h4>상품 이름 : {{ oneroomData[detailCount].title }}</h4>
       <img :src="oneroomData[detailCount].image" style="width: 80%" />
       <p>상품 설명 : {{ oneroomData[detailCount].content }}</p>
-      <p>가격 : {{ oneroomData[detailCount].price }}</p>
+      <!-- <input @input="month =$event.target.value" /> -->
+      <!-- @input대신 v-model을 이용해서 입력된 값을 data에 저장할 수 있다. -->
+      <input v-model="month" />
+      <p>가격 : {{ oneroomData[detailCount].price * month }}</p>
       <button class="btn close-btn" @click="$emit('ModalOnOff')">❌</button>
     </div>
   </div>
@@ -15,6 +18,46 @@
 
 export default {
   name: "ModalWindow",
+  data() {
+    return {
+      month: 1,
+    }
+  },
+  watch: {    //! 감시자 역할
+    month(changedValue) {
+      //! month가 변경될 때 실행할 코드
+
+      //@ 입력한 값이 13 이상이면?
+      if (changedValue >= 13) {
+        alert("12이하의 숫자를 입력해주세요.");
+        this.month = 1; // 초기값을 다시 1로 지정하기
+      }
+
+      //@ 한글, 영어 입력 안되도록 막기
+      // const reg = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|a-z|A-Z]/   // reg = Regular expression(정규식)
+
+      // if (reg.exec(changedValue) !== null) {
+      //   //  = changedValue.slice(0, -1);
+      //   // this.month = changedValue.replace(/[^0-9]/g, '');
+      //   this.month = changedValue.replace(/[^-\0-9]/g, '');
+      // 
+      //@ 한글, 영어 입력되면 알람 띄워주기 => isNaN 이용 (isNaN() 안에 숫자를 입력하면 false, 글자를 입력하면 true가 출력)
+      if (isNaN(changedValue) == true) {
+        alert('문자는 입력이 안되요!');
+        this.month = 1;
+      }
+
+      //@ 스페이스바 입력 못하도록 막기
+      const spaceGap = /\s/; // 공백체크
+
+      if (spaceGap.exec(changedValue)) {
+        alert("공백은 사용할 수 없어요.");
+        this.month = changedValue.replace(' ', '');   // 공백제거
+        this.month = 1; // 초기값을 다시 1로 지정하기
+      }
+
+    }
+  },
   //^ 부모 컴포넌트로부터 받은 데이터를 props에 등록
   props : {               // 형식 => props : { 데이터이름(oneroomData) : 자료형(Array), () : (), ... }
     oneroomData : Array,   
