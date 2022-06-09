65d2f612e4d7c83c8795f0dfbc954db82371f8e8
Takyunho <tyh1819@gmail.com>
2022-06-09 13:30:32 +0900

    1. 새로운 프로젝트 생성 2. vue에서의 style속성 데이터 바인딩 - 콜론(:)과 오브젝트({}) 사용 - {} 사용시 -기호 사용 X -> 카멜케이스로 사용

---
+++ 코딩애플/Vue/vuestargram/src/components/Container.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- <Post :data="data[0]"/>
+    <Post :data="data[1]"/>
+    <Post :data="data[2]"/> -->
+    <!-- 위의 코드 반복문으로 축약 -->
+    <Post :data="data[i]" v-for="(a, i) in data" :key="i"/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue' // Post.vue와 Comtainer.vue는 같은 경로에 있기 때문에 ./만 사용
+
+export default {
+  name: 'Container',
+  data(){
+    return {
+    }
+  },
+  components: {
+    Post: Post,
+  },
+  props: {
+    data: Array,
+  }
+
+}
+  
+
+
+</script>
+
+<style>
+
+</style>
---
+++ 코딩애플/Vue/vuestargram/src/components/Post.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div class="profile" :style="{ backgroundImage : `url(${data.userImage})`}"></div>
+      <!-- style 데이터 바인딩 할때 :붙이고, {} 안에 키 : 값 형태로 가능 ( {{}} X) -->
+      <span class="profile-name">{{data.name}}</span>
+    </div>
+    <div class="post-body" :style="{ backgroundImage : `url(${data.postImage})`}"></div>
+    <div class="post-content">
+      <p>{{data.likes}} Likes</p>
+      <p><strong>{{data.name}}</strong>{{data.content}}</p>
+      <p class="date">May 15</p>
+    </div>
+    
+</div> 
+</template>
+
+<script>
+
+
+export default {
+  name: 'Post',
+  data() {
+    return {
+      
+    }
+  },
+  props: {
+    data : Array,
+  },
+  methods: {
+    
+  }
+  
+}
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  /* background-image: url("https://placeimg.com/100/100/arch"); */
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  /* background-image: url("https://placeimg.com/640/480/animals"); */
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+} 
+</style>
