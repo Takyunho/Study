77d0f7c7d31db4121244d36a4c5112d0b84071a6
Takyunho <tyh1819@gmail.com>
2022-11-07 17:28:08 +0900

    raycaster(클릭감지) - 특정 광선을 지나는 메쉬 체크하기 - 클릭한 메쉬 감지하기

--- 인프런/09/raycaster/src/ex02.js
+++ 인프런/09/raycaster/src/ex02.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { Vector2 } from 'three';
 // 마우스 컨트롤
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
@@ -41,29 +42,20 @@ export default function example() {
 
 	// Controls
 	const controls = new OrbitControls(camera, renderer.domElement);
-	// Mesh
-
-	// 선을 만들때 쓰는 Meterial
-	const lineMaterial = new THREE.LineBasicMaterial({ color: 'yellow'})	
-	const points = [];
-	// 100에서 -100으로 이어지는 선을 만들자
-	points.push(new THREE.Vector3(0, 0, 100));
-	points.push(new THREE.Vector3(0, 0, -100));
-	// 선 geometry 만들기
-	const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-	const guide = new THREE.Line(lineGeometry, lineMaterial);	// geometry 다음에 meterial이여야 함 (순서도 중요)
-	scene.add(guide);
 	
+  // Mesh
 	// 박스 Geometry 만들기
 	const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 	const boxMaterial = new THREE.MeshStandardMaterial({ color: 'plum' });
 	const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+	// console.log(boxMesh)
 	boxMesh.name = "box";	// 아래에서 raycaster로 확인하기 위해서 추가
 
 	// 도넛모양 Geometry 만들기
 	const torusGeometry = new THREE.TorusGeometry(2, 0.5, 16, 100);
 	const torusMaterial = new THREE.MeshStandardMaterial({ color: 'lime' });
 	const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+	console.log(torusMesh)
 	torusMesh.name = 'torus';	// 아래에서 raycaster로 확인하기 위해서 추가
 
 	scene.add(boxMesh, torusMesh)
@@ -71,11 +63,12 @@ export default function example() {
 	// 두 메쉬를 배열에 넣기
 	// 광선에 맞았는지 체크하기 위해 (배열에 넣어놔야 체크하기가 편리함 )
 	const meshes = [boxMesh, torusMesh];
+	// console.log(meshes)
 
-
-	//! 1. RayCaster를 만들어야한다.
+	//! RayCaster
 	const raycaster = new THREE.Raycaster();
-
+  const mouse = new THREE.Vector2();  //* Vector2는 2차원 / 왜 mouse는 Vector2로 만들었을까? => 마우스는 2차원 x, y축으로 이동하기 때문이다.
+  console.log(mouse)
 
 	// 그리기
 	const clock = new THREE.Clock();
@@ -86,41 +79,33 @@ export default function example() {
 		
 		// 일정시간 움직이게 하기
 		boxMesh.position.y = Math.sin(time) * 2;		//* boxMesh의 y축을 sin함수를 이용해서 위아래로 왔다갔다 하도록 함
-		torusMesh.position.y = Math.cos(time) * 2;
+		torusMesh.position.y = Math.sin(time) * 2;
 		// 원래 색상으로 되돌리기
 		boxMesh.material.color.set('plum');
 		torusMesh.material.color.set('lime');
 
-
-		//! 2. raycaster는 시작점과 방향을 지정해주면 된다.
-		const origin = new THREE.Vector3(0, 0, 100);		//# 1. 광선을 쏘는 출발점(시작점)
-		// const direction = new THREE.Vector3(0, 0, -1)		//# 2. 방향 / 방향이니까 각도가 바뀌는 것이 아니므로, -100이 아니라 -1로 해도 됨
-		const direction = new THREE.Vector3(0, 0, -100)		//# -100 으로 하는 경우, normalize()가 필요함
-		direction.normalize();
-
-		//! 원래 raycaster에서 방향을 설정할때는 정규화된 값이여야 한다.
-		//# 3. 시작점과 방향을 정했으면 광선 세팅하기
-		raycaster.set(origin, direction);
-		//# test. console로 광선 맞았는지 확인하기
-		const intersects = raycaster.intersectObjects(meshes);
-		// console.log(intersects)	// meshes 배열에 있는 mesh를 가지고 체크를 하게 됨 / 앞면과 뒷면 다 맞아서 [{},{}] 가 출력
-
-		intersects.forEach(item => {
-			console.log(item.object.name);
-			item.object.material.color.set('red');	// color의 set 메소드로 색 세팅
-		})
-
-		
-		
-
-
-
-
-
-
 		renderer.render(scene, camera);
 		renderer.setAnimationLoop(draw);
 	}
+
+
+  //^ 클릭시 실행할 함수
+  function checkIntersects() {
+    raycaster.setFromCamera(mouse, camera);  //* 카메라 시점에서 광선을 쐈을때 즉, 카메라 위치를 origin으로 잡아서 마우스가 클릭한 지점으로 광선을 세팅함.
+
+    const intersects = raycaster.intersectObjects(meshes);
+    
+    for (const item of intersects) {
+      console.log(item.object.name);
+      break;    // 여러개를 관통해서 클릭하고 싶으면 break 제거 / 하나만 클릭하고 싶으면 break 걸어야 함
+    }
+    // if (intersects[0]) {
+    //   console.log(intersects[0].object.name);
+    // }
+    //! if문을 걸어서 intersects[0] 처럼 하는 것과 for of 반복문을 돌려서 하는 경우 성능상 차이는 거의 없음.
+    //! 다만, for of 반복문을 통해서 구현하는 경우, break를 제거하여 여러개의 meshe를 관통해서 클릭할 수 있음
+  }
+
 
 	function setSize() {
 		camera.aspect = window.innerWidth / window.innerHeight;
@@ -129,8 +114,18 @@ export default function example() {
 		renderer.render(scene, camera);
 	}
 
-	// 이벤트
+	//^ 이벤트
 	window.addEventListener('resize', setSize);
+  canvas.addEventListener('click', e => {
+    // console.log(e);
+    // console.log(e.clientX, e.clientY);
 
+    // three.js에 맞게 좌표 변환하기  (raycaster에서 쓰려면 좌표변환이 필요함)  (가운데가 0)
+    mouse.x = e.clientX / canvas.clientWidth * 2 - 1;
+    mouse.Y = -(e.clientY / canvas.clientHeight * 2 - 1);   // 앞에 -를 붙여줘야 함
+    console.log(mouse);
+    
+    checkIntersects();
+  })
 	draw();
 }
