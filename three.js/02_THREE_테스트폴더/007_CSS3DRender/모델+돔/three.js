8543ce9795ed3cb1399fbc458b446caaf7f7bca6
takyunho <tyh1819@gmail.com>
2022-11-29 09:47:42 +0900

    Feat : 추가 플로틀리차트

--- three.js/02_THREE_테스트폴더/007_CSS3DRender/모델+돔/three.js
+++ three.js/02_THREE_테스트폴더/007_CSS3DRender/모델+돔/three.js
@@ -169,7 +169,7 @@ function init() {
 
     getDataAndDrawChart('myPlot1', 5110, '45773-4C000'); // 눌렀을 때 파라미터에 pcd, icd 전달
     getDataAndDrawChart('myPlot2', 5110, '45940-2F200'); // 눌렀을 때 파라미터에 pcd, icd 전달
-    getDataAndDrawChart('myPlot3', 5111, '31667 X160A'); // 눌렀을 때 파라미터에 pcd, icd 전달
+    getDataAndDrawChart2('myPlot3'); // 눌렀을 때 파라미터에 pcd, icd 전달
     // getDataAndDrawChart('myPlot4', 5110, '45773-4C000'); // 눌렀을 때 파라미터에 pcd, icd 전달
     jQuery('#bg').show();
     chartContainer.classList.remove('none');
@@ -483,7 +483,152 @@ function getDataAndDrawChart(plot, pcd, icd) {
 
 
 // 제니코스 차트 그리기
+const url = `http://14.52.100.115:5000/rest/regression/predict?work_line=MA05&sensor=homo_rpm`
+    // ex. work_line : MA05 / sensor : homo_rpm
 
+
+    function getDataAndDrawChart2(plot) {
+      // 데이터 얻어오기 (api 호출)
+      const getData = async () => {
+        const response = await fetch(url);
+        const resJSON = await response.json();
+        return resJSON;
+      }
+
+      getData().then((result) => {
+        console.log("result : ", result);
+        result.reverse(); // 시간값이 반대로 나와서 차트 그릴때 x축을 위해 reverse() 함수를 이용하여 배열 뒤집기
+
+            // 데이터 가공
+            let nowDate = [];
+            let nowResult = [];
+            let predictHigh = [];
+            let predictLow = [];
+            let sensor = [];
+            let workLine = [];
+
+            result.forEach((item, index) => {
+              nowDate[index] = item.now_date;
+              nowResult[index] = item.now_result;
+              predictHigh[index] = item.predict_high;
+              predictLow[index] = item.predict_low;
+              sensor[index] = item.sensor;
+              workLine[index] = item.work_line;
+            })
+            // console.log("now_date : ", nowDate);
+            // console.log("now_result : ", nowResult)
+            // console.log("predict_high : ", predictHigh)
+            // console.log("predict_low : ", predictLow)
+            // console.log("sensor : ", sensor)
+            // console.log("work_line : ", workLine)
+            // console.log("status : ", status)
+
+            // 차트 그리기 함수
+            drawChart(plot, nowDate, nowResult, predictHigh, predictLow, sensor);
+
+        
+        
+      })
+
+
+      // 차트 그리기함수
+      function drawChart(plot, nowDate, nowResult, predictHigh, predictLow, sensor) {
+
+        let data = [];
+
+        // 시작시간 => 08시부터
+        // const today = new Date();
+        const START = moment(nowDate, 'YYYY-MM-DD').format('YYYY-MM-DD 08:00:00');
+        // 종료시간 => 21시까지
+        // const endTime = new Date(today);
+        const END = moment(nowDate, 'YYYY-MM-DD').format('YYYY-MM-DD 21:00:00');
+
+
+        /** 상한과 하한안에 현재값이 들어간 것처럼 음영을 주기 위해서는,
+        * fill과 fillcolor를 줘야함
+        * 그리고 상한에만 fill과 fillcolor를 주고 하한에는 안주는 것이 포인트
+        * 또, 트레이스의 순서도 중요하다. 현재 기준으로 순서는 하한/현재/상한으로 지정
+        */
+        // predictHigh(상한값)
+        const trace1 = {
+          x: nowDate,
+          y: predictHigh,
+          type: 'scatter',
+          mode: 'lines+markers',
+          name: '상한',
+          line: { width: 2, color: "red" },
+          marker: { color: "red", size: 2 },
+          // fill: "tonexty",
+          // fillcolor: "rgba(211, 211, 211, 0.5)",
+          // fillcolor: "rgba(68, 68, 68, 0.1)",
+        };
+
+        // predictLow(하한값)
+        const trace2 = {
+          x: nowDate,
+          y: predictLow,
+          type: 'scatter',
+          mode: 'lines+markers',
+          name: '하한',
+          line: { width: 2 },
+          marker: { color: "red", size: 2 },
+          fill: "tonexty",
+          // fillcolor: 'rgba(0,0,0,0)'
+          fillcolor: 'rgba(211, 211, 211, 0.5)'
+        }
+        
+        // nowResult(현재값)
+        const trace3 = {
+          x: nowDate,
+          y: nowResult,
+          type: 'scatter',
+          mode: 'lines+markers',
+          name: '현재',
+          line: { color: '#2196f3', width: 2 },
+          marker: { color: "#2196f3", size: 2 },
+          // fill: "tonexty",
+          // fillcolor: "rgba(24,29,41)"
+        };
+        
+        data.push(trace1, trace2, trace3);
+
+        // 레이아웃
+        const layout = {
+          title: false,   // 차트의 제목
+          // width: 507,     // 차트의 너비
+          height: 145,    // 차트의 높이
+          xaxis: {
+            range: [START, END],
+            zeroline: false,
+            ticks: "inside",              // 글씨를 안쪽으로 할지 바깥쪽으로 할지
+            gridcolor: 'rgba(125, 127, 132, 0.4)',  // x축의 grid 색상을 변경할 수 있다.
+            // tickcolor: 'rgba(0,0,0,0)',
+            nticks: 6
+          },
+          yaxis: {
+            zeroline: false,
+            ticks: "outside",
+            gridcolor: 'rgba(125, 127, 132, 0.4)',
+            nticks: 5,
+          },
+
+          margin: { t: 30, b: 55, l: 55, r: 20, pad: 10 },
+          showlegend: false,
+          paper_bgcolor: 'rgba(0,0,0,0)',
+          plot_bgcolor: 'rgba(0,0,0,0)',
+          autosize: true,
+          font: {
+            // family: 'Noto Sans KR',
+            size: 12,
+            color: '#fff',
+            weight: 200
+          },
+
+        }
+
+        Plotly.newPlot(plot, data, layout, { displayModeBar: false });
+      }
+    }
 
 
 
