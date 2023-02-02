61365f8de66e06ab7e1cf6e04f58b54a307fc81c
Takyunho <tyh1819@gmail.com>
2023-02-02 19:43:58 +0900

    feat: click했을 때 선 긋기

--- 노마드코더/바닐라JS_그림앱/app.js
+++ 노마드코더/바닐라JS_그림앱/app.js
@@ -4,19 +4,16 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 간단한 사람모양 만들기
-// 팔
-ctx.fillRect(210, 300, 15, 100)
-ctx.fillRect(330, 300, 15, 100)
-// 몸통
-ctx.fillRect(250, 300, 55, 200)
-// 머리 (동그라미를 그릴때는 arc를 사용한다.)
-ctx.arc(278, 250, 40, 0, 2 * Math.PI);    // 시작 각도 0 / 끝 각도 2 * Math.PI
-ctx.fill();
+// 클릭하면 선긋기
+ctx.lineWidth = 1;
+ctx.moveTo(0, 0)
 
-// 눈
-ctx.beginPath();    // * style을 지정하기 위해서는 꼭 경로를 새로 지정해야 한다.
-ctx.fillStyle = "#fff";
-ctx.arc(258, 250, 5, Math.PI, 2 * Math.PI);
-ctx.arc(298, 250, 5, Math.PI, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  console.log(event)
+  // ctx.moveTo(0, 0)
+  ctx.lineTo(event.offsetX, event.offsetY)
+  ctx.stroke();
+}
+
+canvas.addEventListener("click", onClick);
+// canvas.addEventListener("mousemove", onClick);
