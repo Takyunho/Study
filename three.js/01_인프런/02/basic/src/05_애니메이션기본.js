0c1a83bfea8f6b7ea375317894993f1c132ca9ac
Takyunho <tyh1819@gmail.com>
2023-02-20 14:43:33 +0900

    comment: 주석 수정

--- three.js/01_인프런/02/basic/src/05_애니메이션기본.js
+++ three.js/01_인프런/02/basic/src/05_애니메이션기본.js
@@ -46,11 +46,13 @@ export default function example() {
 
 	// 그리기
 	function draw() {
-		// 0.1(각도)은 Radian(라디안)을 사용
+		// 0.1(각도)은 Radian(라디안)을 사용s
 		// 360도는 2파이 (2 * 파이 = 6.3이므로, 6.3이 360도라고 보면 됨) 
 		mesh.rotation.y += 0.1; 
 
-		mesh.rotation.y += THREE.MathUtils.degToRad(1);		// MathUtils.degToRad(각도) => 우리가 아는 degree(각도)를 Radian(라디안)으로 변환해주는 three.js 내장함수
+		mesh.rotation.y += THREE.MathUtils.degToRad(1);
+		// MathUtils.degToRad(각도)
+		// 우리가 아는 degree(각도)를 Radian(라디안)으로 변환해주는 three.js 내장함수
 
 		renderer.render(scene, camera);
 	
