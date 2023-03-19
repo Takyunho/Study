70abc52d0900abb54f70fce3834d940bfa28f258
Takyunho <tyh1819@gmail.com>
2023-03-19 20:08:06 +0900

    add: life cycle
    - created()와 mounted()를 주로 사용한다.
    - 그 외에 beforeCreate(), beforeMount 등은 간혹가다가 한 번 사용함

---
+++ 패스트캠퍼스/Vue_study/02_lifecycle/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  beforeCreate() {
+    console.log("beforeCreate", this.count) // create 전에는 undefined
+  },
+  created() {
+    console.log("Created", this.count)  // 2
+  },
+  beforeMount() {
+    console.log("BeforeMount")
+    console.log(document.querySelector("h1")) // 마운트 되기 전에는 null
+  },
+  mounted() {
+    console.log("mounted")
+    console.log(document.querySelector("h1")) // <h1>2</h1>
+  }
+}
+</script>
+
+<style>
+
+</style>
