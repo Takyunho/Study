2cb8a547f0fbed6ae88e8052acbda573ab52d590
Takyunho <tyh1819@gmail.com>
2023-03-23 10:54:29 +0900

    add: material을 각지게 표현하기

--- 인프런/three.js/07_material(재질)/material/src/main.js
+++ 인프런/three.js/07_material(재질)/material/src/main.js
@@ -1,5 +1,6 @@
 // import example from './ex01_MeshBasicMaterial';
 // import example from './ex02_Lambert_Phong';
-import example from './ex03_Phong_Standard';
+// import example from './ex03_Phong_Standard';
+import example from './ex04_각지게 표현하기';
 
 example();
