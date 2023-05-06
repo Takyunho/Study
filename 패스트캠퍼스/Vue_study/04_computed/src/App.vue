e0123280da5c7be50f9d1cab73fe0353eb6ee454
Takyunho <tyh1819@gmail.com>
2023-05-06 14:40:56 +0900

    Computed 란?

---
+++ 패스트캠퍼스/Vue_study/04_computed/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
