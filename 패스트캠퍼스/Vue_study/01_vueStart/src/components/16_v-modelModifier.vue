46168a8123e26721253895b6156fe6f8483fa489
Takyunho <tyh1819@gmail.com>
2023-05-09 15:21:14 +0900

    v-model 수식어2
    - 라디오 버튼 사용시 v-model="picked"
    - 체크박스 버튼 사용시 v-model="checked"
    - 체크박스 사용시 :true-value, :false-value
    - textarea 사용시 v-model 사용
    - selected 사용시 v-model 사용

--- 패스트캠퍼스/Vue_study/01_vueStart/src/components/16_v-modelModifier.vue
+++ 패스트캠퍼스/Vue_study/01_vueStart/src/components/16_v-modelModifier.vue
@@ -17,9 +17,48 @@
     <!-- trim()이라는 메소드는 문자의 제일 앞과 뒤의 공백을 제거시켜주는 메소드인데,
     v-model.trim으로 간단하게 구현 가능하다. -->
     <input type="text" v-model.trim="msg2">
-    
+
     <!-- 메소드 체이닝으로 연결도 가능! -->
     <input type="text" v-model.lazy.trim="msg2">
+
+    <!-- 5. radio 버튼 -->
+    <!-- radio 버튼을 사용할 때는 v-model="picked" 사용 -->
+    <!-- data에서 picked를 정의해야되는데, 값은 빈 문자열이여도 상관없다. -->
+    <input type="radio" value="1" v-model="picked" />
+    <label for="one">하나</label>
+    <input type="radio" value="2" v-model="picked" />
+    <label for="two">둘</label>
+
+    <!-- 6. checkbox 버튼 -->
+    <!-- 체크박스는 checked값에 불리언타입 데이터를 사용 -->
+    <!-- 예를들어 `toggle`은 true 또는 false -->
+    <div> {{ toggle }}</div>
+    <input type="checkbox" v-model="toggle" />
+
+    <!-- true-value 및 false-value 속성은 v-model을 사용하는 경우에만 작동하는 Vue 전용 속성 -->
+    <!-- toggle 속성의 값은 체크박스가 선택되면 네로 설정되고 선택되지 않을 때는 아니오로 설정된다. -->
+    <input
+        type="checkbox"
+        v-model="toggle"
+        :true-value="dynamicTrueValue"
+        :false-value="dynamicFalseValue" />
+
+
+    <!-- 7. textarea -->
+    <!-- textarea를 사용할 때는 이중 중괄호 X / v-model 사용 필요 -->
+    <!-- <textarea>{{ msg }}</textarea> -->
+    <textarea v-model="msg"></textarea>
+
+    <!-- 8. selected -->
+    <div>선택됨: {{ selected }}</div>
+
+    <select v-model="selected">
+        <option disabled value="">다음 중 하나를 선택하세요</option> <!--disabled 속성을 설정하면 select 못함-->
+        <option>가</option>
+        <option>나</option>
+        <option>다</option>
+    </select>
+
 </template>
 
 <script>
@@ -28,6 +67,12 @@ export default {
         return {
             msg: 123,
             msg2: "hello world!",
+            picked: '',
+            toggle: false,
+            dynamicTrueValue: '네',
+            dynamicFalseValue: '아니오',
+            selected: '',
+
         }
     },
     watch: {    // watch는 data의 값이 변경될 때마다 실행된다.
@@ -39,6 +84,12 @@ export default {
             // console.log(this.msg2.trim())   // trim()이라는 메소드는 문자의 제일 앞과 뒤의 공백을 제거시켜주는 메소드
 
             console.log(this.msg2)      // v-model.trim을 사용하는 경우 msg2의 값중에서 앞과 뒤의 공백만 제거하고 값은 변경되지 않았다고 판단하기 떄문에 콘솔이 출력되지 않는다.
+        },
+        picked() {
+            console.log(this.picked)
+        },
+        selected() {
+            console.log(this.selected)
         }
     },
 }
