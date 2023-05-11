3876d24d7241471cfa8eb50430b1ae2e41442a16
Takyunho <tyh1819@gmail.com>
2023-05-11 10:34:57 +0900

    반응형 데이터
    - setup 메소드에서 정의한 기본적인 변수는 반응성을 가지지 않는다.
    따라서 vue 패키지에서 제공하는 ref 속성을 import한 후, 그 ref속성을 변수에 할당해서 사용해야 한다.
    ref속성을 통해 할당된 변수는 객체 데이터이므로 .을 이용해 뽑아써야한다.
    그리고 return을 통해 반응성을 갖는 변수와 함수를 리턴해줘야 한다.

--- 패스트캠퍼스/Vue_study/02_compositionapi/src/App.vue
+++ 패스트캠퍼스/Vue_study/02_compositionapi/src/App.vue
@@ -3,17 +3,22 @@
   <OptionApi></OptionApi>
   <!-- 컴포넌트 api 사용 -->
   <CompositionApi></CompositionApi>
+
+  <!-- 2. 반응형 데이터 -->
+  <Responsive></Responsive>
 </template>
 
 <script>
 import OptionApi from './components/01_optionAPI.vue'
 import CompositionApi from './components/01_compositionAPI.vue'
+import Responsive from './components/02_반응형데이터.vue'
 
 export default {
   name: 'App',
   components: {
     OptionApi,
-    CompositionApi
+    CompositionApi,
+    Responsive,
   }
 }
 </script>
---
+++ 패스트캠퍼스/Vue_study/02_compositionapi/src/components/02_반응형데이터.vue
@@ -0,0 +1,50 @@
+<template>
+    <div @click="increase">
+        {{ count }}
+    </div>
+</template>
+
+<script>
+//=> composition api를 사용할 때는 반응성을 가질 수 있도록 vue.js에서 제공하는 ref라는 속성을 사용해야 하므로, vue 패키지에서 import 해야 함
+import { ref } from 'vue';
+
+export default {
+    // => option api
+    // data() {
+    //     return {
+    //         count: 0,
+    //     }
+    // },
+    // methods: {
+    //     increase() {
+    //         this.count += 1;
+    //     }
+    // },
+
+    // option api로 작성된 내용을 composition api로 변경하기
+    // => composition api
+    
+    setup() {
+        // let count = 0;       //- setup 메소드에서 정의한 기본적인 변수는 반응성을 가지지 않는다.
+        let count = ref(0);     //- 반응성을 가질 수 있는 객체데이터가 count에 할당됨
+        console.log(count)  
+
+        function increase() {
+            count.value += 1;         //- ref로 인해 할당된 객체 데이터에서 value를 뽑아써야함
+        }
+
+        return {
+            count,
+            increase
+        }
+    },
+
+    watch: {
+        count() {
+            console.log(this.count)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
