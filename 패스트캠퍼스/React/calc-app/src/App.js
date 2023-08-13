5f6fdac453249843492095a938ca5675afe860ce
Takyunho <tyh1819@gmail.com>
2023-08-13 22:06:53 +0900

    Feat: state 생성 및 setState를 이용하여 리렌더링 시키기

--- 패스트캠퍼스/React/calc-app/src/App.js
+++ 패스트캠퍼스/React/calc-app/src/App.js
@@ -4,16 +4,24 @@ import ExpenseForm from './components/ExpenseForm';  // ExpenseForm 컴포넌트
 import ExpenseList from './components/ExpenseList';  // ExpenseList 컴포넌트를 import 한다.
 
 class App extends Component {
-
-  initialExpense = [
-    { id: 1, charge: '렌트비', amount: 1010 },
-    { id: 2, charge: '교통비', amount: 2400 },
-    { id: 3, charge: '식비', amount: 3600 },
-  ]
+  //* state 생성하기
+  constructor(props) {
+    super(props);
+    this.state = {
+      expense: [
+        { id: 1, charge: '렌트비', amount: 1010 },
+        { id: 2, charge: '교통비', amount: 2400 },
+        { id: 3, charge: '식비', amount: 3600 },
+      ]
+    }
+  }
 
   handleDelete = (id) => {
-    const newExpenses = this.initialExpense.filter(expense => expense.id !== id)
+    const newExpenses = this.state.expense.filter(expense => expense.id !== id)
     console.log(newExpenses);
+    // this.setState를 이용하여 state 업데이트 하기 => 리렌더링 시키기
+    // this.setState({업데이트 전 상태 : 업데이트 시킬 상태})
+    this.setState({expense: newExpenses})
   }
 
   render() {
@@ -30,7 +38,7 @@ class App extends Component {
           {/* Expense List */}
           {/* props로 데이터 전달하기 */}
           <ExpenseList
-            initialExpense={this.initialExpense}
+            initialExpense={this.state.expense}
             //* 함수도 props로 전달할 수 있다.
             handleDelete={this.handleDelete}
           />
