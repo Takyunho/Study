68b1c2cd3bb20e47cfcf516e5f0f481f5d41a1a2
Takyunho <tyh1819@gmail.com>
2023-08-18 23:47:42 +0900

    Add: JSX문법 심화
    - jsx에서는 변수와 같은 데이터를 이용하는 경우 무조건 {}를 써야함!!

--- DreamCoding/basic/src/App.js
+++ DreamCoding/basic/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const list = ['딸기', '바나나', '우유', '초코']  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello React!</h1>
+      <ul>
+        {
+          // 1.
+          list.map(item => <li>{item}</li>)
+          // 2.
+          // list.map(item => (
+          //   <li>{item}</li>
+          // ))
+          // 3.
+          // list.map((item) => {
+          //   return <li>{item}</li>;
+          // })
+          // 4.
+          // list.map(function (item) {
+          //   return <li>{item}</li>;
+          // })
+        }
+      </ul>
+    </>
   );
 }
 
