1266d8cbe43f3fe3865af361b6e4f7b78917bcce
Takyunho <tyh1819@gmail.com>
2023-08-19 15:15:42 +0900

    Feat: 조건에 따라 렌더링하기
    - isNew가 true인 경우에 new 태그 보여주기
    - Boolean값은 {}로 묶어서 보내야 한다.

--- DreamCoding/basic/src/AppProfile.jsx
+++ DreamCoding/basic/src/AppProfile.jsx
@@ -7,12 +7,19 @@ export default function AppProfile() {
       <Profile
         image="https://images.unsplash.com/photo-1602033350291-a9ab8d800269?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fCVFQyU5NiVCQyVFQSVCNSVCNHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
         name="rina"
+        // newTag="new"
+        isNew={true}  // Boolean값은 {}로 묶어서 보낸다.
         title="프론트엔드 고수"
       ></Profile>
       <Profile
         image="https://images.unsplash.com/photo-1691573479026-14dbac630c88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
         name="Anonymous"
         title="프론트엔드 초보"
+      ></Profile>
+      <Profile
+        image="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
+        name="Andrew"
+        title="백엔드 개발자"
       ></Profile>
     </>
   );
--- DreamCoding/basic/src/components/Profile.jsx
+++ DreamCoding/basic/src/components/Profile.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-// 상위 컴포넌트에서 App.css를 import해왔기 때문에 하위 컴포넌트인 Profile.jsx에서 class를 주더라도 스타일이 정의된다..!!
 
-// export default function Profile(props) {
-export default function Profile({ image, name, title }) {
+export default function Profile({ image, /* newTag */ name, title, isNew }) {
   return (
     <div className="profile">
-      {/* <img className="photo" src={props.image} alt="avatar" /> */}
-      {/* <h1>{props.name}</h1> */}
-      {/* <p>{props.title}</p> */}
       <img className="photo" src={image} alt="avatar" />
+      {/* //^ 1. 3항 연산자로 newTag가 전달된 컴포넌트만 class주기 */}
+      {/* <span className={ newTag ? 'new-tag' : '' }>{ newTag }</span> */}
+      
+      {/* //^ 2. isNew의 참 거짓 여부에 따라 span 보여주기 / 안보여주기 */}
+      {/* isNew가 true이고, span태그가 존재하면 참 */}
+      {isNew && <span className="new-tag">New</span>}
+      
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
