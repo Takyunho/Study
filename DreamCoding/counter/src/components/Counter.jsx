d632bbe488ae588719fbea5b98d72adfba3f7233
Takyunho <tyh1819@gmail.com>
2023-08-19 17:26:19 +0900

    Feat: useState 주의할 점
    - 해당 코드에서 setCount를 사용할 때, 여러번 업데이트 해야 되거나 이전 상태값을 업데이트 해야 되는 경우에는 콜백함수의 리턴값을 이용해야 함

--- DreamCoding/counter/src/components/Counter.jsx
+++ DreamCoding/counter/src/components/Counter.jsx
@@ -4,8 +4,30 @@ export default function Counter() {
   const [count, setCount] = useState(0);
 
   const addNumber = () => {
-    setCount(count + 1); 
-    console.log(count)
+    /* 
+    만약에 setCount를 여러번 실행하면?!
+
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+    // => 1이 출력됨
+
+    5가 나올거같지만 1이 나옴
+    왜냐하면 addNumber 함수가 실행될 때 count는 0으로 고정되어버리기 때문(closure)
+    */
+    
+    /* 
+    따라서 아래처럼 콜백함수를 줘서 사용해야 함 
+    (콜백 안의 이름은 이전값을 의미하는 단어면 상관없음) 
+    */
+    setCount((이전카운트값) => { return 이전카운트값 + 1})
+    setCount((이전카운트값) => 이전카운트값 + 1)
+    setCount((이전카운트값) => 이전카운트값 + 1)
+    setCount((이전카운트값) => 이전카운트값 + 1)
+    setCount((이전카운트값) => 이전카운트값 + 1)
+    console.log(count)    // 5, 10, 15 ...가 찍힘
   }
 
   return (
