4c5950da832360d567a0af53805868ed6b0c85f9
Takyunho <tyh1819@gmail.com>
2023-08-19 14:45:30 +0900

    Feat: props를 이용하여 여러개의 컴포넌트 동적으로 재사용하기
    - 자식 컴포넌트 함수명에 props를 매개변수로 받아서 사용
    - 부모 컴포넌트에서는 키="값" 형태로 데이터 전송
    - 객체구조분해할당 문법을 통해 props 분해 가능

--- DreamCoding/basic/src/AppProfile.jsx
+++ DreamCoding/basic/src/AppProfile.jsx
@@ -1,11 +1,19 @@
-import './App.css';
-import Profile from './components/Profile';
+import "./App.css";
+import Profile from "./components/Profile";
 
 export default function AppProfile() {
-  
   return (
     <>
-      <Profile></Profile>
+      <Profile
+        image="https://images.unsplash.com/photo-1602033350291-a9ab8d800269?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fCVFQyU5NiVCQyVFQSVCNSVCNHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        name="rina"
+        title="프론트엔드 고수"
+      ></Profile>
+      <Profile
+        image="https://images.unsplash.com/photo-1691573479026-14dbac630c88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        name="Anonymous"
+        title="프론트엔드 초보"
+      ></Profile>
     </>
   );
 }
--- DreamCoding/basic/src/components/Profile.jsx
+++ DreamCoding/basic/src/components/Profile.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 // 상위 컴포넌트에서 App.css를 import해왔기 때문에 하위 컴포넌트인 Profile.jsx에서 class를 주더라도 스타일이 정의된다..!!
 
-export default function Profile() {
+// export default function Profile(props) {
+export default function Profile({ image, name, title }) {
   return (
-    <div className='profile'>
-      <img className='photo' src="https://images.unsplash.com/photo-1602033350291-a9ab8d800269?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fCVFQyU5NiVCQyVFQSVCNSVCNHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="avatar" />
-      <h1>Lina</h1>
-      <p>프론트엔드 고수</p>
+    <div className="profile">
+      {/* <img className="photo" src={props.image} alt="avatar" /> */}
+      {/* <h1>{props.name}</h1> */}
+      {/* <p>{props.title}</p> */}
+      <img className="photo" src={image} alt="avatar" />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
-
