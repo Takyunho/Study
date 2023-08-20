6954a6f1e071a2787ce60e193613a798e6e3cb98
Takyunho <tyh1819@gmail.com>
2023-08-20 21:35:40 +0900

    Feat: checkbox 체크된 경우에 다른 데이터 불러오기
    - useEffect의 두번째 인자로 값이 들어있는 디펜던시를 보내서 그 값이 변경될 때마다 useEffect 재실행 되도록 함
    -> checked 상태가 변경됨에따라 useEffect가 실행됨
    
    - 삼항연산자를 이용해 fetch의 url부분을 다르게 불러옴

--- DreamCoding/life-cycle/src/components/Products.jsx
+++ DreamCoding/life-cycle/src/components/Products.jsx
@@ -1,38 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => setChecked((checked) => !checked)
 
   useEffect(() => {
-    //^ 경로를 그냥쓰면 public 폴더에서 찾는다.
-    fetch("data/products.json")
+    fetch(`data/${ checked ? 'sale_' : '' }products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("네트워크에서 받아온 데이터", data);
         setProducts(data);
       });
 
-    //^ useEffect에서 return의 콜백함수를 통해 cleanup을 할 수 있다.
     return () => {
       console.log("Products 컴포넌트가 unmounted 될때 실행됩니다.");
     };
-  }, []);   //^ 첫번째 인자는 콜백, 두번째 인자는 디펜던시를 전달(빈 배열을 전달하면 아무런 디펜던시가 전달되지 않기 때문에 컴포넌트가 처음에 보여질때만 useEffect훅이 실행된다.)
+  }, [checked]);  //^ checked가 변경될때 useEffect 훅이 다시 실행됨
 
   return (
     <div>
+      <input id='checkBox' type="checkbox" value={checked} onChange={handleChange} />
+      <label htmlFor="checkBox">Show only hot sale!</label>
       <ul>
-        {/* {products.map(product => {
-          return (
-            <li key={product.id}>
-              <article>
-                <h3>{product.name}</h3>
-                <p>{product.price}</p>
-              </article>
-            </li>
-          )
-        })} */}
-
         {products.map((product) => (
           <li key={product.id}>
             <article>
@@ -42,7 +33,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </div>
   );
 }
