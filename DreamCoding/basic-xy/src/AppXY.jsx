e4a7a7b670565460efed7409130c9e7038fc4993
Takyunho <tyh1819@gmail.com>
2023-08-25 16:49:56 +0900

    Feat: spread 연산자를 이용해서 여러개의 값 그대로 가져오기

--- DreamCoding/basic-xy/src/AppXY.jsx
+++ DreamCoding/basic-xy/src/AppXY.jsx
@@ -2,32 +2,21 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  // const [X, setX] = useState(0);
-  // const [Y, setY] = useState(0);
-  const [position, setPosition] = useState({x: 0, y: 0})
+  // const [position, setPosition] = useState({x: 0, y: 0})
+  const [position, setPosition] = useState({x: 0, y: 0, z: 0})
 
   const mouseMove = (e) => {
-    // console.log(e)
-    // setX(e.clientX)
-    // setY(e.clientY)
-    // console.log(X, Y)
-    
-    // setPosition({ x: e.clientX, y: e.clientY })
-    // console.log(position.x, position.y)
-
     // 만약에 수평으로만 이동이 가능하다면?
-    setPosition((prev) => ({ x: e.clientX, y: prev.y }))
+    // setPosition((prev) => ({ x: e.clientX, y: prev.y }))
+
+    // 만약에 객체에 여러개의 값이 담겨있는 경우
+    // x의 값만 변경하고 싶다면 나머지 키와 값들은 spread 연산자를 사용하여 그대로 가져오고, x만 수정!
+    setPosition((prev) => ({ ...prev, x: e.clientX }))
   }
 
   return (
     <div className='container' onPointerMove={mouseMove}>
-      {/* react에서 인라인 style prop에서 픽셀 값을 넣을 때, 자동으로 숫자 뒤에 px을 붙여준다! */}
-      {/* <div className='pointer' style={{ left: X - '15', top: Y - '15' }}></div> */}
-      {/* <div className='pointer' style={{ transform: `translate(${X}px, ${Y}px)` }}></div> */}
-
       <div className='pointer' style={{ transform: `translate(${position.x}px, ${position.y}px)`}}></div>
-      
-
     </div>
   );
 }
