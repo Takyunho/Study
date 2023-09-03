f33844191121be609c88fa7a680d76b145c9b234
Takyunho <tyh1819@gmail.com>
2023-09-03 18:43:20 +0900

    Feat: react에서 form 다루기

---
+++ DreamCoding/app-form/src/AppForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './App.css';
+
+export default function AppForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">이름</label>
+      <input type="text" id='name' name='name' value={name} onChange={(event) => {
+        setName(event.target.value);
+      }} />
+      <label htmlFor="email">이메일</label>
+      <input type="email" id='email' name='email' value={email} onChange={(e) => {
+        setEmail(e.target.value);
+      }} />
+      <button>submit</button>
+    </form>
+  );
+}
