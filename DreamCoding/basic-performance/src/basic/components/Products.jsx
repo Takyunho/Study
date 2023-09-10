1f58293692d4a82632e91401302c0658ca05f641
Takyunho <tyh1819@gmail.com>
2023-09-10 20:50:22 +0900

    Feat: loading, err msg 구현

--- DreamCoding/basic-performance/src/basic/components/Products.jsx
+++ DreamCoding/basic-performance/src/basic/components/Products.jsx
@@ -5,18 +5,42 @@ export default function Products() {
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
 
+  //* loading, error 표시하기
+  const [loading, setLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState();
+
+  //* loading, error 표시하기
   useEffect(() => {
+    //* 1 
+    setLoading(true);
+    setErrMsg(undefined);
+
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+        // setLoading(false); // finally()를 사용하므로, 이 코드는 필요없다.
         setProducts(data);
-      });
+      }).catch(() => {
+        //* 2
+        setErrMsg('데이터를 가져오는데 실패했습니다.');
+      }).finally(() => setLoading(false)); //* 3. 성공하든 실패하든 로딩은 끝나야 하므로, finally()를 사용한다.
+
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
   }, [checked]);
 
+
+
+  //* 4. if문을 return해서 여러가지의 내용을 표시할 수 있다!!
+  // 데이터를 가져오고 있는 경우,
+  if (loading) return <p>로딩중...!</p>
+  
+  // 데이터를 가져오는데 실패한 경우,
+  if (errMsg) return <p>{errMsg}</p>
+
+  // 데이터를 가져오는데 성공한 경우,
   return (
     <>
       <input
