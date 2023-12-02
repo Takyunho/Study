e771f7aa4cc2082891918deb8855fa310efed2ed
Takyunho <tyh1819@gmail.com>
2023-12-02 16:41:36 +0900

    Feat: 4.14 Event 처리하기
    - react에서 event는 SyntheticBaseEvent로 한번 감싸진 상태로 들어온다.
    - event처리시 함수를 호출해서는 안된다. 함수를 호출하면 return문이 실행되면서 함수가 호출돼버린다.

--- DreamCoding/4.1-4.22/src/AppProfile.jsx
+++ DreamCoding/4.1-4.22/src/AppProfile.jsx
@@ -8,8 +8,17 @@ export default function AppProfile() {
   // 컴포넌트의 이름은 대문자로 시작하고, 카멜케이스로 작성한다.
   // 그러나 Next.js에서는 App-jsx 이런식으로 사용하기도 함
 
+  //@ 4.14 Event 처리하기
+  const handleClick = (event) => {
+    //^ react에서 event는 SyntheticBaseEvent로 한번 감싸진 상태로 들어온다.
+    console.log(event)
+    alert("클릭됨!")
+  }
   return (
     <>
+      <button onClick={handleClick}>버튼</button>
+      {/* <button onClick={handleClick()}>버튼</button> 처럼 함수를 호출해서는 안된다!!! */}
+
       {/* 4.9 props 사용 */}
       <Profile
         image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
