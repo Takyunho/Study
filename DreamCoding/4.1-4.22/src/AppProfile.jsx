51d9d96ecf6b860887f095ec4c65238a94469320
Takyunho <tyh1819@gmail.com>
2023-12-02 16:07:47 +0900

    Feat: 4.10~4.11 새로운 개발자 태그 만들기
    - isNew가 true인 경우에만 new라는 뱃지 보여주기

--- DreamCoding/4.1-4.22/src/AppProfile.jsx
+++ DreamCoding/4.1-4.22/src/AppProfile.jsx
@@ -14,11 +14,13 @@ export default function AppProfile() {
         image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
         name="James Kim"
         title="프론트엔드 개발자"
+        isNew={true}
       ></Profile>
       <Profile
         image="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
         name="kim James"
         title="백엔드 개발자"
+        isNew={false}
       ></Profile>
       <Profile
         image="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
--- DreamCoding/4.1-4.22/src/components/Profile.jsx
+++ DreamCoding/4.1-4.22/src/components/Profile.jsx
@@ -4,14 +4,12 @@ import React from 'react';
 // AppProfile.jsx 즉 부모 컴포넌트에서 App.css를 import 해왔기 때문에,
 // 자식 컴포넌트에서 따로 import 할 필요가 없다.
 
-export default function Profile({image, name, title}) {
+export default function Profile({image, name, title, isNew}) {
   return (
-    <div className='profile'>
-      <img
-        src={image}
-        alt="avatar"
-        className='photo'
-      />
+    <div className="profile">
+      <img src={image} alt="avatar" className="photo" />
+      {/* isNew가 true인 경우에만 span태그를 보여준다. */}
+      {isNew && <span className="new">NEW</span>}
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
