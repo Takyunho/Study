95c2bc3ebd3d669ea24fa384c6306b88324ba0d7
Takyunho <tyh1819@gmail.com>
2023-12-02 20:56:15 +0900

    Feat: 4.20 useEffect 제대로 사용하기
    - useEffect의 디펜던시에 값을 넣어서 그 값이 변경될 때마다 호출하도록 하기
    - useEffect는 화면 초기로드시 값을 가져오거나, 무거운 작업을 수행할 때 쓴다.

--- DreamCoding/4.1-4.22/src/components/Products.jsx
+++ DreamCoding/4.1-4.22/src/components/Products.jsx
@@ -1,40 +1,40 @@
 import React, { useEffect, useState } from "react";
 
-//@ 4.19 useEffect 생애주기
+//@ 4.20 useEffect 제대로 사용하기
 
 export default function Products() {
-  const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
 
-  // fetch("data/products.json") // 그냥 상대경로를 쓰면 public으로 접근 가능
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log("데이터 가져오기 성공!");
-  //     setProducts(data);
-  //   });
+  const handleCheck = () => {
+    setChecked(prev => !prev)
+  }
 
-  // 위처럼 컴포넌트 내에서 그냥 fetch api를 사용하면 무한 루프에 걸린다.
-  //@ 따라서 화면이 처음 로드될 때 딱 한번만 호출하도록 하려면 useEffect를 사용하면 된다.
-  //^ useEffect(콜백함수, 디펜던시)
-  // 👆 첫번째 인자는 콜백, 두번째 인자는 디펜던시를 전달(빈 배열을 전달하면 아무런 디펜던시가 전달되지 않기 때문에 컴포넌트가 처음에 보여질때만 useEffect훅이 실행된다.)
   useEffect(() => {
-    fetch("data/products.json")
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("데이터 가져오기 성공!");
+        console.log(checked);
         setProducts(data);
       });
 
-    //# 컴포넌트가 없어질 때 메모리를 정리한다던지, 소켓 네트워크 통신을 닫는다던지 할때에는 return 함수를 사용한다.
     return () => {
       console.log(
         "return 함수는 컴포넌트가 사라질 때(unmount될 때) 호출됩니다."
       );
     };
-  }, []);
+  }, [checked]);  // checked가 변경될 때마다 호출
 
   return (
     <div>
+      <input
+        id="checkbox"
+        type="checkbox"
+        value={checked}
+        onChange={handleCheck}
+      />
+      <label htmlFor="checkbox">show only Sale 😆</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
