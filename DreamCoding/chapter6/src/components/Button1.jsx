c90d5615a8d4d371d75eb94d85a025775155792d
Takyunho <tyh1819@gmail.com>
2023-12-14 08:52:17 +0900

    Feat: 6.3 PostCSS

---
+++ DreamCoding/chapter6/src/components/Button1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button1.module.css';
+
+export default function Button1() {
+  return (
+    <>
+      {/* <button className='button'>Button1</button> */}
+      <button className={styles.button}>Button1</button>
+    </>
+  );
+}
+
--- DreamCoding/chapter6/src/index.js
+++ DreamCoding/chapter6/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* 6.3 PostCSS */}
     <App />
   </React.StrictMode>
 );
